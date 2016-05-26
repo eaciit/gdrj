0458c9feb3f14aa642f3fd438007a17a3f34dbf7
Arief Darmawan <ariefda@hotmail.com>
2016-05-26 14:10:45 +0700

    ledger summary update

--- model/ledgersummary.go
+++ model/ledgersummary.go
@@ -1,8 +1,11 @@
 package gdrj
 
-import "github.com/eaciit/toolkit"
-
-//"github.com/eaciit/dbox"
+import (
+    "github.com/eaciit/toolkit"
+    "github.com/eaciit/dbox"
+    "strings"
+    "errors"
+)
 
 type LedgerSummary struct {
 	ID                     string
@@ -22,4 +25,73 @@ func (s *LedgerSummary) RecordID() interface{} {
 
 func (s *LedgerSummary) TableName() string {
 	return "LedgerSummary"
+}
+
+func SummarizeLedgerSum(
+    filter *dbox.Filter,
+    columns []string, 
+    datapoints []string, 
+    fnTransform func(m *toolkit.M)error)([]toolkit.M, error){
+        
+    sum := new(LedgerSummary)
+    conn := DB().Connection
+    q := conn.NewQuery().From(sum.TableName())
+    if filter!=nil {
+        q = q.Where(filter)
+    }
+    if len(columns)>0{
+        q = q.Group(columns...)
+    }
+    if len(datapoints)==0{
+        return nil, errors.New("SummarizedLedgerSum: Datapoints should be defined at least 1")
+    }
+    for _, dp := range datapoints{
+        dps := strings.Split(dp,":")
+        if len(dps)<2{
+            return nil, errors.New("SummarizeLedgerSum: Parameters should follow this pattern aggrOp:fieldName:[alias - optional]")
+        }
+        op := ""
+        if !strings.HasPrefix(dps[0],"$"){
+            dps[0]="$" + strings.ToLower(dps[0])
+        }
+        if toolkit.HasMember([]string{dbox.AggrSum, dbox.AggrAvr, dbox.AggrMax, 
+            dbox.AggrMin, dbox.AggrMean, dbox.AggrMed}, dps[0]){
+            op=dps[0]        
+        }
+        if op=="" {
+            return nil, errors.New("SummarizeLedgerSum: Invalid Operation")
+        }
+        field := dps[1]
+        alias := field
+        if len(dps)>2{
+            alias=dps[2]
+        }
+        q = q.Aggr(op, field, alias)
+    }
+    
+    c, e := q.Cursor(nil)
+    if e!=nil {
+        return nil, errors.New("SummarizedLedgerSum: Preparing cursor error " + e.Error())
+    }
+    defer c.Close()
+    
+    ms := []toolkit.M{}
+    if c.Count()>0{
+        e = c.Fetch(&ms, 0, false)
+        if e!=nil {
+            return nil, errors.New("SummarizedLedgerSum: Fetch cursor error " + e.Error())
+        }
+    }
+    
+    if fnTransform!=nil {
+        for idx, m := range ms{
+            e = fnTransform(&m)
+            if e!=nil {
+                return nil, errors.New(toolkit.Sprintf("SummarizedLedgerSum: Transform error on index %d, %s", 
+                    idx, e.Error()))
+            }
+        }
+    }
+    
+    return ms, nil
 }
