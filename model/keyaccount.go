916f9482ee3d8371ae1373523e32c77b0253ee95
alipsidikp <alip.sidik.p@gmail.com>
2016-05-27 10:28:35 +0700

    customer

---
+++ model/keyaccount.go
@@ -0,0 +1,43 @@
+package gdrj
+
+import (
+	"errors"
+	"github.com/eaciit/orm/v1"
+	"github.com/eaciit/toolkit"
+)
+
+type KeyAccount struct {
+	orm.ModelBase `json:"-" bson:"-"`
+	ID            string `json:"_id" bson:"_id"` //KeyAccount
+	Name          string
+}
+
+func (k *KeyAccount) RecordID() interface{} {
+	return k.ID
+}
+
+func (k *KeyAccount) TableName() string {
+	return "keyaccount"
+}
+
+func KeyAccountGetByID(id string) *KeyAccount {
+	b := new(KeyAccount)
+	DB().GetById(b, id)
+	return b
+}
+
+func (k *KeyAccount) Save() error {
+	err := Save(k)
+	if err != nil {
+		return errors.New(toolkit.Sprintf("[%v-%v] Error found : %v ", k.TableName(), "save", err.Error()))
+	}
+	return err
+}
+
+func (k *KeyAccount) Delete() error {
+	err := Delete(k)
+	if err != nil {
+		return errors.New(toolkit.Sprintf("[%v-%v] Error found : %v ", k.TableName(), "delete", err.Error()))
+	}
+	return err
+}
