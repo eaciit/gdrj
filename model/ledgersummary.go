6414f5feddc3b625333da12bcef5053f86efbcb5
Arief Darmawan <ariefda@hotmail.com>
2016-05-27 09:23:54 +0700

    sum

--- model/ledgersummary.go
+++ model/ledgersummary.go
@@ -1,14 +1,17 @@
 package gdrj
 
 import (
-    "github.com/eaciit/toolkit"
-    "github.com/eaciit/dbox"
-    "strings"
-    "errors"
+	"errors"
+	"strings"
+
+	"github.com/eaciit/dbox"
+	"github.com/eaciit/orm"
+	"github.com/eaciit/toolkit"
 )
 
 type LedgerSummary struct {
-	ID                     string
+	orm.ModelBase          `bson:"-" json:"-"`
+	ID                     string `bson:"_id"`
 	PC                     *ProfitCenter
 	CC                     *CostCenter
 	CompanyCode            string
@@ -20,7 +23,13 @@ type LedgerSummary struct {
 }
 
 func (s *LedgerSummary) RecordID() interface{} {
-	return toolkit.Sprintf("%d_%d_%s_%s", s.Date.Year, s.Date.Month, s.CompanyCode, s.LedgerAccount)
+	return s.ID
+    //return toolkit.Sprintf("%d_%d_%s_%s", s.Date.Year, s.Date.Month, s.CompanyCode, s.LedgerAccount)
+}
+
+func (s *LedgerSummary) PrepareID() interface{}{
+    s.ID = toolkit.Sprintf("%d_%d_%s_%s", s.Date.Year, s.Date.Month, s.CompanyCode, s.LedgerAccount)
+    return s
 }
 
 func (s *LedgerSummary) TableName() string {
@@ -32,76 +41,78 @@ func (s *LedgerSummary) TableName() string {
     {_id:{col1:"D1",col2:"D2",col3:"D3"},SalesAmount:10,Qty:5,Value:2},
     {_id:{col1:"D1",col2:"D2",col3:"D4"},SalesAmount:10,Qty:3.2,Value:3},
 ]
-
 row: _id.col1, _id.col2
 col: _id.col3
-
 */
 func SummarizeLedgerSum(
-    filter *dbox.Filter,
-    columns []string, 
-    datapoints []string, 
-        // misal: ["sum:Value1:SalesAmount","sum:Value2:Qty","avg:Value3"]
-    fnTransform func(m *toolkit.M)error)([]toolkit.M, error){
-    sum := new(LedgerSummary)
-    conn := DB().Connection
-    q := conn.NewQuery().From(sum.TableName())
-    if filter!=nil {
-        q = q.Where(filter)
-    }
-    if len(columns)>0{
-        q = q.Group(columns...)
-    }
-    if len(datapoints)==0{
-        return nil, errors.New("SummarizedLedgerSum: Datapoints should be defined at least 1")
-    }
-    for _, dp := range datapoints{
-        dps := strings.Split(dp,":")
-        if len(dps)<2{
-            return nil, errors.New("SummarizeLedgerSum: Parameters should follow this pattern aggrOp:fieldName:[alias - optional]")
+	filter *dbox.Filter,
+	columns []string,
+	datapoints []string,
+	// misal: ["sum:Value1:SalesAmount","sum:Value2:Qty","avg:Value3"]
+	fnTransform func(m *toolkit.M) error) ([]toolkit.M, error) {
+	sum := new(LedgerSummary)
+	conn := DB().Connection
+	q := conn.NewQuery().From(sum.TableName())
+	if filter != nil {
+		q = q.Where(filter)
+	}
+	if len(datapoints) == 0 {
+		return nil, errors.New("SummarizedLedgerSum: Datapoints should be defined at least 1")
+	}
+	for _, dp := range datapoints {
+		dps := strings.Split(dp, ":")
+		if len(dps) < 2 {
+			return nil, errors.New("SummarizeLedgerSum: Parameters should follow this pattern aggrOp:fieldName:[alias - optional]")
+		}
+		fieldid := dps[1]
+        alias := fieldid
+		op := ""
+		if !strings.HasPrefix(dps[0], "$") {
+			dps[0] = "$" + strings.ToLower(dps[0])
+		} 
+        
+        if toolkit.HasMember([]string{dbox.AggrSum, dbox.AggrAvr, dbox.AggrMax,
+			dbox.AggrMin, dbox.AggrMean, dbox.AggrMed}, dps[0]) {
+			op = dps[0]
+		}
+		if op == "" {
+			return nil, errors.New("SummarizeLedgerSum: Invalid Operation")
+		}
+		if len(dps) > 2 {
+			alias = dps[2]
+		}
+		
+        if strings.HasPrefix(alias,"$") {
+            alias = alias[1:]
         }
-        op := ""
-        if !strings.HasPrefix(dps[0],"$"){
-            dps[0]="$" + strings.ToLower(dps[0])
-        }
-        if toolkit.HasMember([]string{dbox.AggrSum, dbox.AggrAvr, dbox.AggrMax, 
-            dbox.AggrMin, dbox.AggrMean, dbox.AggrMed}, dps[0]){
-            op=dps[0]        
-        }
-        if op=="" {
-            return nil, errors.New("SummarizeLedgerSum: Invalid Operation")
-        }
-        field := dps[1]
-        alias := field
-        if len(dps)>2{
-            alias=dps[2]
-        }
-        q = q.Aggr(op, field, alias)
+		
+        q = q.Aggr(op, fieldid, alias)
+	}
+    if len(columns) > 0 {
+		q = q.Group(columns...)
+	}
+	
+	c, e := q.Cursor(nil)
+	if e != nil {
+		return nil, errors.New("SummarizedLedgerSum: Preparing cursor error " + e.Error())
+	}
+	defer c.Close()
+
+	ms := []toolkit.M{}
+    e = c.Fetch(&ms, 0, false)
+    if e != nil {
+        return nil, errors.New("SummarizedLedgerSum: Fetch cursor error " + e.Error())
     }
-    
-    c, e := q.Cursor(nil)
-    if e!=nil {
-        return nil, errors.New("SummarizedLedgerSum: Preparing cursor error " + e.Error())
-    }
-    defer c.Close()
-    
-    ms := []toolkit.M{}
-    if c.Count()>0{
-        e = c.Fetch(&ms, 0, false)
-        if e!=nil {
-            return nil, errors.New("SummarizedLedgerSum: Fetch cursor error " + e.Error())
-        }
-    }
-    
-    if fnTransform!=nil {
-        for idx, m := range ms{
-            e = fnTransform(&m)
-            if e!=nil {
-                return nil, errors.New(toolkit.Sprintf("SummarizedLedgerSum: Transform error on index %d, %s", 
-                    idx, e.Error()))
-            }
-        }
-    }
-    
-    return ms, nil
+	
+	if fnTransform != nil {
+		for idx, m := range ms {
+			e = fnTransform(&m)
+			if e != nil {
+				return nil, errors.New(toolkit.Sprintf("SummarizedLedgerSum: Transform error on index %d, %s",
+					idx, e.Error()))
+			}
+		}
+	}
+
+	return ms, nil
 }
---
+++ test/summary/z0_test.go
@@ -0,0 +1,81 @@
+package testsummary
+
+import (
+    "testing"
+    "github.com/eaciit/toolkit"
+    "github.com/eaciit/dbox"
+    "github.com/eaciit/orm"
+    _ "github.com/eaciit/dbox/dbc/mongo"
+    "eaciit/gdrj/model"
+    "strings"
+    "os"
+)
+
+var (
+    _db dbox.IConnection
+    e error
+)
+
+func DB()dbox.IConnection{
+    if _db==nil {
+        ci := &dbox.ConnectionInfo{"localhost:27123", "ecgodrej", "", "", toolkit.M{}.Set("timeout", 3)}
+        conn, err := dbox.NewConnection("mongo",ci)
+        
+        if err!=nil{
+            toolkit.Println("Error:", err.Error())
+            os.Exit(200)
+        }
+
+        err = conn.Connect()
+        if err!=nil {
+            toolkit.Println("Error:", err.Error())
+            os.Exit(200)
+        }
+        _db = conn
+        gdrj.SetDb(_db)
+     }
+    return _db
+}
+
+var _ctx *orm.DataContext
+
+func Ctx() *orm.DataContext{
+    _ctx = orm.New(DB())
+    return _ctx
+}
+
+func Test(t *testing.T) {
+    ctx := Ctx()
+    for i:=1;i<=1000;i++{
+        s := new(gdrj.LedgerSummary)
+        s.CompanyCode="C01"
+        s.Date = &gdrj.Date{Year:2015,Month:4,}
+        s.LedgerAccount = toolkit.Sprintf("%d",i)
+        if len(s.LedgerAccount)<10{
+            s.LedgerAccount=strings.Repeat("0",10-len(s.LedgerAccount)) + s.LedgerAccount
+        }
+        s.Value1 = toolkit.ToFloat64(toolkit.RandInt(1000000),0,toolkit.RoundingAuto)
+        s.Value2 = toolkit.ToFloat64(toolkit.RandInt(1000),0,toolkit.RoundingAuto)
+        e = ctx.Save(s)    
+        if e!=nil {
+            t.Fatalf("Error saving record %d: %s\n%s",i,toolkit.JsonString(s),e.Error())
+        }  
+    }
+}
+
+func TestSummarize(t *testing.T) {
+    ms, e := gdrj.SummarizeLedgerSum(nil,
+        []string{"companycode"},
+        []string{"sum:$value1"},
+        nil)
+   if e!=nil {
+       t.Fatalf("Error: %s", e.Error())
+   }
+   toolkit.Println(ms)
+}
+
+func TestClose(t *testing.T){
+    if DB()!=nil{
+        DB().Close()
+    }
+}
