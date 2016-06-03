7d46f4018025d09c767a9ec0c836b5bf59fcbd54
novalagung <caknopal@gmail.com>
2016-06-03 16:16:28 +0700

    no message

--- web/assets/core/js/page-report-analysis.js
+++ web/assets/core/js/page-report-analysis.js
@@ -2,3 +2,14 @@
 
 vm.currentTitle(o.Name);
 vm.breadcrumb(vm.breadcrumb().concat([{ title: o.Name, href: o.ID }]));
+
+vm.reportAnalysis = {};
+var ra = vm.reportAnalysis;
+
+rpt.init = function () {
+	pvt.init();
+};
+
+rpt.refresh = function () {
+	pvt.refreshData();
+};
--- web/src/js/page-report-analysis.js
+++ web/src/js/page-report-analysis.js
@@ -2,3 +2,14 @@ vm.currentTitle(o.Name)
 vm.breadcrumb(vm.breadcrumb().concat([
 	{ title: o.Name, href: o.ID }
 ]))
+
+vm.reportAnalysis = {}
+let ra = vm.reportAnalysis
+
+rpt.init = () => {
+	pvt.init()
+}
+
+rpt.refresh = () => {
+	pvt.refreshData()
+}
