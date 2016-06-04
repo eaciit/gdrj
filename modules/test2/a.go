256a554bb25fd67467642e11fd243699b121fd2f
Arief Darmawan <ariefda@hotmail.com>
2016-06-04 18:05:24 +0700

    salestrx

--- modules/test2/a.go
+++ modules/test2/a.go
@@ -9,6 +9,7 @@ import (
 	"github.com/eaciit/dbox"
 	"github.com/eaciit/orm/v1"
 	"github.com/eaciit/toolkit"
+    "time"
 	// "strings"
 )
 
@@ -32,6 +33,7 @@ var (
     ccs = toolkit.M{}
     ledgers = toolkit.M{}
     prods = toolkit.M{}
+    custs = toolkit.M{}
 )
 
 func getCursor(obj orm.IModel)dbox.ICursor{
@@ -46,13 +48,14 @@ func prepMaster(){
     pc:=new(gdrj.ProfitCenter)
     cc:=new(gdrj.CostCenter)
     prod:=new(gdrj.Product)
-    ledger:=new(gdrj.LedgerAccount)
+    ledger:=new(gdrj.LedgerMaster)
     
     cpc := getCursor(pc)
     defer cpc.Close()
     var e error
     for e=cpc.Fetch(pc,1,false);e==nil;{
         pcs.Set(pc.ID,pc)
+        pc =new(gdrj.ProfitCenter)
         e=cpc.Fetch(pc,1,false)
     }
     
@@ -60,6 +63,7 @@ func prepMaster(){
     defer ccc.Close()
     for e=ccc.Fetch(cc,1,false);e==nil;{
         ccs.Set(cc.ID,cc)
+        cc = new(gdrj.CostCenter)
         e=ccc.Fetch(cc,1,false)
     }
     
@@ -67,6 +71,7 @@ func prepMaster(){
     defer cprod.Close()
     for e=cprod.Fetch(prod,1,false);e==nil;{
         prods.Set(prod.ID,prod)
+        prod=new(gdrj.Product)
         e=cprod.Fetch(prod,1,false)
     }
     
@@ -74,11 +79,18 @@ func prepMaster(){
     defer cledger.Close()
     for e=cledger.Fetch(ledger,1,false);e==nil;{
         prods.Set(ledger.ID,prod)
+        ledger=new(gdrj.LedgerMaster)
         e=cledger.Fetch(ledger,1,false)
     }
     
-    toolkit.Printfn("Populate data:\n" +
-        toolkit.JsonString(prods))
+    cust := new(gdrj.Customer)
+    ccust:=getCursor(cust)
+    defer ccust.Close()
+    for e=ccust.Fetch(cust,1,false);e==nil;{
+        custs.Set(cust.ID,cust)
+        cust=new(gdrj.Customer)
+        e=ccust.Fetch(cust,1,false)
+    }
 }
 
 func main() {
@@ -86,5 +98,90 @@ func main() {
 	setinitialconnection()
 	defer gdrj.CloseDb()
     
+    toolkit.Println("Reading Master")
     prepMaster()
+    
+    toolkit.Println("START...")
+
+	//for i, src := range arrstring {
+	//dbf := dbox.Contains("src", src)
+	crx, err := gdrj.Find(new(gdrj.RawDataPL), nil, nil)
+	if err != nil {
+		toolkit.Println("Error Found : ", err.Error())
+		os.Exit(1)
+	}
+
+    arrcount := []int{0}
+	i := 0
+	arrcount[i] = crx.Count()
+	func(xi int, xcrx dbox.ICursor) {
+		ci := 0
+		iseof := false
+		for !iseof {
+			arrpl := []*gdrj.RawDataPL{}
+			_ = xcrx.Fetch(&arrpl, 1000, false)
+
+			if len(arrpl) < 1000 {
+				iseof = true
+			}
+
+			for _, v := range arrpl {
+				tdate := time.Date(v.Year, time.Month(v.Period), 1, 0, 0, 0, 0, time.UTC).AddDate(0, 3, 0)
+
+				ls := new(gdrj.LedgerSummary)
+				ls.CompanyCode = v.EntityID
+				ls.LedgerAccount = v.Account
+
+				ls.Year = tdate.Year()
+				ls.Month = tdate.Month()
+				ls.Date = &gdrj.Date{
+					Month: tdate.Month(),
+					Year:  tdate.Year()}
+
+				ls.PCID = v.PCID
+				if v.PCID != "" && pcs.Has(v.PCID) {
+					ls.PC = pcs.Get(v.PCID).(*gdrj.ProfitCenter)
+				}
+
+				ls.CCID = v.CCID
+				if v.CCID != "" && ccs.Has(v.CCID) {
+					ls.CC = ccs.Get(v.CCID).(*gdrj.CostCenter)
+				}
+
+				ls.OutletID = v.OutletID
+				if v.OutletID != "" && custs.Has(v.OutletID) {
+					ls.Customer = custs.Get(v.OutletID).(*gdrj.Customer)
+                    //ls.Customer = gdrj.CustomerGetByID(v.OutletID)
+				}
+
+				ls.SKUID = v.SKUID
+				if v.SKUID != "" && prods.Has(v.SKUID) {
+                    ls.Product = prods.Get(v.SKUID).(*gdrj.Product)
+				}
+
+				ls.Value1 = v.AmountinIDR
+				ls.Value2 = v.AmountinUSD
+
+                tLedgerAccount := new(gdrj.LedgerMaster)
+                if ledgers.Has(ls.LedgerAccount){
+				    tLedgerAccount = ledgers.Get(ls.LedgerAccount).(*gdrj.LedgerMaster)
+                }
+				ls.PLCode = tLedgerAccount.PLCode
+				ls.PLOrder = tLedgerAccount.OrderIndex
+
+				ls.ID = ls.PrepareID().(string)
+				err = gdrj.Save(ls)
+				if err != nil {
+					toolkit.Println("Error Found : ", err.Error())
+					os.Exit(1)
+				}
+
+				ci++
+			}
+
+			toolkit.Printfn("%d of %d Processed", ci, arrcount[xi])
+		}
+
+	}(i, crx)
+	toolkit.Println("END...")
 }
