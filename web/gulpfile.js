ed366ea8b1250e13ef96d6f724e301d421ed786d
novalagung <caknopal@gmail.com>
2016-06-06 14:04:43 +0700

    no message

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -4,12 +4,14 @@ const gulp    = require('gulp')
 const gutil   = require('gulp-util')
 const babel   = require('gulp-babel')
 const less    = require('gulp-less')
+const compass = require('gulp-compass')
 const path    = require('path')
 const del     = require('del')
 
 const baseSourcePath = 'src'
 const baseDestPath   = 'assets/core'
 const sourcePathJS   = `${baseSourcePath}/js/**.js`
+const sourcePathSASS = `${baseSourcePath}/sass/**.sass`
 const sourcePathLESS = `${baseSourcePath}/less/**.less`
 const destPathJS     = `${baseDestPath}/js`
 const destPathCSS    = `${baseDestPath}/css`
@@ -40,5 +42,16 @@ gulp.task('less:watch', ['less'], () => {
 	gulp.watch(sourcePathLESS, ['less'])
 })
 
+gulp.task('compass', () => {
+	gulp.src(sourcePathSASS)
+		.pipe(compass({ css: destPathCSS, sass: './src/sass' }).on('error', gutil.log))
+		.pipe(gulp.dest(destPathCSS))
+})
+
+gulp.task('compass:watch', ['compass'], () => {
+	gulp.watch(sourcePathSASS, ['compass'])
+})
+
+// let tasks = ['clean', 'babel:watch', 'compass:watch']
 let tasks = ['clean', 'babel:watch', 'less:watch']
 gulp.task('default', tasks, noop)
