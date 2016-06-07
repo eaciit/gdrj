9607163888ed913b4fc65da98dbe1331cc3d5139
Arfian Bagus <arfianbagus@Arfians-MacBook-Pro.local>
2016-06-08 06:52:04 +0700

    no message

--- web/assets/core/js/page-widget-chart-scatter.js
+++ web/assets/core/js/page-widget-chart-scatter.js
@@ -7,89 +7,172 @@ var dataPoints = [{ field: "value1", name: "value1", aggr: "sum" }];
 rs.contentIsLoading = ko.observable(false);
 rs.title = ko.observable('P&L Analytic');
 rs.breakdownBy = ko.observable('customer.channelname');
-rs.pplheader = ko.observable('Net Sales');
+rs.pplheader = ko.observable('EBIT');
+rs.datascatter = ko.observableArray([]);
 
-rs.optionDimensionSelect = ko.observableArray([
-// { field: 'Net Sales', name: 'Net Sales' },
-{ field: 'EBIT', name: 'EBIT' }, { field: 'Cost of Goods Sold', name: 'Cost of Goods Sold' }]);
+rs.optionDimensionSelect = ko.observableArray([]);
 
-rs.refresh = function () {
-    var dimensions = [{ "field": "plmodel.plheader1", "name": "plheader1" }, { "field": rs.breakdownBy(), "name": "Channel" }, { "field": "year", "name": "Year" }];
-    var dataPoints = [{ field: "value1", name: "value1", aggr: "sum" }];
-    var param = rpt.wrapParam(dimensions, dataPoints);
-    param.filters.push({
-        "Op": "$eq",
-        "Field": "plmodel.plheader1",
-        "Value": rs.pplheader()
-    });
-    console.log(param);
-    app.ajaxPost("/report/summarycalculatedatapivot", param, function (res) {
-        if (!app.isFine(res)) {
-            return;
-        }
-        console.log(res);
-    });
+rs.getSalesHeaderList = function () {
+	app.ajaxPost("/report/GetSalesHeaderList", {}, function (res) {
+		var data = Lazy(res).map(function (k, v) {
+			return { field: k._id['plmodel.plheader1'], name: k._id['plmodel.plheader1'] };
+		}).toArray();
+		rs.optionDimensionSelect(data);
+		rs.optionDimensionSelect.remove(function (item) {
+			return item.field == 'Net Sales';
+		});
+		rs.pplheader('EBIT');
+		rs.refresh();
+	});
 };
 
-rs.generateReport = function (data1, data2) {
-    $(".scatter-view").kendoChart({
-        title: {
-            text: ""
-        },
-        legend: {
-            visible: false
-        },
-        seriesDefaults: {
-            type: "line",
-            missingValues: "gap"
-        },
-        // stack: {
-        //     type: "100%"
-        // }
-        series: [{
-            name: "Gold Medals",
-            data: [40, 40, 40, 40, 40, 40, 40, null, 40, 40, 40, 40, 40, 40, 40],
-            color: "#f3ac32"
-        }, {
-            name: "Aye",
-            data: [19, 25, 21, 26, 28, 31, 35, null, 60, 31, 34, 32, 24, 40, 38],
-            color: "red",
-            // line: {
-            // 	opacity: 0,
-            // 	width: 0
-            // },
-            opacity: 0,
-            markers: {
-                type: 'cross',
-                size: 8
-            }
-        }],
-        valueAxis: {
-            line: {
-                visible: false
-            },
-            minorGridLines: {
-                visible: true
-            },
-            label: {
-                template: "#: value #%"
-            }
-        },
-        categoryAxis: {
-            categories: ['aaa', 'aaaa', 'aaaa', 'aaaa \n 2015', '', '', '', '', '', '', '', '2016', '', '', ''],
-            majorGridLines: {
-                visible: false
-            }
-        },
-        tooltip: {
-            visible: true,
-            template: "#= series.name #: #= value #"
-        }
-    });
+rs.refresh = function () {
+	setTimeout(function () {
+		var dimensions = [{ "field": "plmodel.plheader1", "name": "plheader1" }, { "field": rs.breakdownBy(), "name": "Channel" }, { "field": "year", "name": "Year" }];
+		var dataPoints = [{ field: "value1", name: "value1", aggr: "sum" }];
+		var param = rpt.wrapParam(dimensions, dataPoints);
+		param.filters.push({
+			"Op": "$eq",
+			"Field": "plmodel.plheader1",
+			"Value": rs.pplheader()
+		});
+		app.ajaxPost("/report/summarycalculatedatapivot", param, function (res) {
+			var dataall = Lazy(res.Data).groupBy(function (f) {
+				return f['year'];
+			}).map(function (k, v) {
+				return { _id: v, data: k };
+			}).toArray();
+
+			param.filters = [];
+			param.filters.push({
+				"Op": "$eq",
+				"Field": "plmodel.plheader1",
+				"Value": 'Net Sales'
+			});
+			app.ajaxPost("/report/summarycalculatedatapivot", param, function (res2) {
+				var dataall2 = Lazy(res2.Data).groupBy(function (f) {
+					return f['year'];
+				}).map(function (k, v) {
+					return { _id: v, data: k };
+				});
+
+				rs.datascatter([]);
+				var title = Lazy(rpt.optionDimensions()).findWhere({ field: rs.breakdownBy() }).title;
+				for (var i in dataall) {
+					var currentDataAll = Lazy(dataall).findWhere({ _id: dataall[i]._id });
+					var currentDataAll2 = Lazy(dataall2).findWhere({ _id: dataall[i]._id });
+
+					var totalDataAll = Lazy(currentDataAll.data).sum(function (e) {
+						return e.value1;
+					});
+					var totalDataAll2 = Lazy(currentDataAll2.data).sum(function (e) {
+						return e.value1;
+					});
+
+					var maxNetSales = Lazy(currentDataAll2.data).max(function (e) {
+						return e.value1;
+					}).value1;
+					var percentage = totalDataAll / totalDataAll2;
+					var v = maxNetSales * 100;
+					var meanYear = (dataall[i].data.length / 2).toFixed(0) - 1;
+					var titleValue = '';
+
+					for (var a in dataall[i].data) {
+						titleValue = dataall[i].data[a][title];
+						if (a == meanYear) titleValue = titleValue + '\n ' + dataall[i].data[a].year;
+						rs.datascatter.push({
+							pplheader: percentage * 100,
+							value1: dataall[i].data[a].value1 / maxNetSales * 100,
+							title: titleValue,
+							header: dataall[i].data[a].plmodel_plheader1,
+							year: dataall[i].data[a].year
+						});
+					}
+					if (i == 0) {
+						rs.datascatter.push({
+							pplheader: null,
+							value1: null,
+							title: '',
+							header: null
+						});
+					}
+				}
+				rs.generateReport();
+			});
+		});
+	});
+};
+
+rs.generateReport = function () {
+	$(".scatter-view").kendoChart({
+		dataSource: {
+			data: rs.datascatter()
+		},
+		title: {
+			text: ""
+		},
+		legend: {
+			visible: false
+		},
+		seriesDefaults: {
+			type: "line",
+			missingValues: "gap"
+		},
+		// stack: {
+		//     type: "100%"
+		// }
+		series: [{
+			name: "PPL",
+			field: 'pplheader',
+			color: "#f3ac32",
+			tooltip: {
+				visible: true,
+				template: "#: dataItem.title # - #: kendo.toString(dataItem.pplheader, 'pplheader') #"
+			}
+		}, {
+			name: "PPL",
+			color: "red",
+			field: "value1",
+			opacity: 0,
+			markers: {
+				type: 'cross',
+				size: 8
+			},
+			tooltip: {
+				visible: true,
+				template: "#: dataItem.title # - #: kendo.toString(dataItem.value1, 'n2') #"
+			}
+		}],
+		valueAxis: {
+			line: {
+				visible: false
+			},
+			minorGridLines: {
+				visible: true
+			},
+			label: {
+				template: "#: value #%"
+			}
+		},
+		categoryAxis: {
+			field: 'title',
+			majorGridLines: {
+				visible: false
+			}
+		},
+		//       labels: {
+		// 	rotation: 60
+		// }
+		tooltip: {
+			visible: true,
+			template: "#= series.name #: #= value #"
+		}
+	});
 };
 
 $(function () {
-    rpt.value.From(moment("2015-02-02").toDate());
-    rpt.value.To(moment("2016-02-02").toDate());
-    rs.refresh();
+	rpt.value.From(moment("2015-02-02").toDate());
+	rpt.value.To(moment("2016-02-02").toDate());
+	// rs.refresh()
+	rs.getSalesHeaderList();
 });
--- web/src/js/page-widget-chart-scatter.js
+++ web/src/js/page-widget-chart-scatter.js
@@ -7,40 +7,109 @@ let dataPoints = [
 rs.contentIsLoading = ko.observable(false)
 rs.title = ko.observable('P&L Analytic')
 rs.breakdownBy = ko.observable('customer.channelname')
-rs.pplheader = ko.observable('Net Sales')
+rs.pplheader = ko.observable('EBIT')
+rs.datascatter = ko.observableArray([])
 
-rs.optionDimensionSelect = ko.observableArray([
-	// { field: 'Net Sales', name: 'Net Sales' },
-    { field: 'EBIT', name: 'EBIT' },
-	{ field: 'Cost of Goods Sold', name: 'Cost of Goods Sold' },
-])
+rs.optionDimensionSelect = ko.observableArray([])
 
-rs.refresh = () => {
-	let dimensions = [
-		{ "field": "plmodel.plheader1", "name": "plheader1" },
-		{ "field": rs.breakdownBy(), "name": "Channel" },
-		{ "field": "year", "name": "Year" }
-	]
-	let dataPoints = [
-		{field: "value1", name: "value1", aggr: "sum"}
-	]
-	let param = rpt.wrapParam(dimensions, dataPoints)
-	param.filters.push({
-	    "Op": "$eq",
-	    "Field": "plmodel.plheader1",
-	    "Value": rs.pplheader()
-	})
-	console.log(param)
-	app.ajaxPost("/report/summarycalculatedatapivot", param, (res) => {
-		if (!app.isFine(res)) {
-			return;
-		}
-		console.log(res)
+rs.getSalesHeaderList = () => {
+	app.ajaxPost(`/report/GetSalesHeaderList`, {}, (res) => {
+		let data = Lazy(res)
+			.map((k, v) => { 
+				return {field: k._id['plmodel.plheader1'], name: k._id['plmodel.plheader1']}
+			})
+			.toArray()
+		rs.optionDimensionSelect(data)
+		rs.optionDimensionSelect.remove( (item) => { return item.field == 'Net Sales'; } )
+		rs.pplheader('EBIT')
+		rs.refresh()
 	})
 }
 
-rs.generateReport = (data1, data2) => {
+rs.refresh = () => {
+	setTimeout(function () {
+		let dimensions = [
+			{ "field": "plmodel.plheader1", "name": "plheader1" },
+			{ "field": rs.breakdownBy(), "name": "Channel" },
+			{ "field": "year", "name": "Year" }
+		]
+		let dataPoints = [
+			{field: "value1", name: "value1", aggr: "sum"}
+		]
+		let param = rpt.wrapParam(dimensions, dataPoints)
+		param.filters.push({
+		    "Op": "$eq",
+		    "Field": "plmodel.plheader1",
+		    "Value": rs.pplheader()
+		})
+		app.ajaxPost("/report/summarycalculatedatapivot", param, (res) => {
+			let dataall = Lazy(res.Data)
+				.groupBy((f) => f['year'])
+				.map((k, v) => { 
+					return { _id: v, data: k } 
+				})
+				.toArray()
+
+			param.filters = []
+			param.filters.push({
+			    "Op": "$eq",
+			    "Field": "plmodel.plheader1",
+			    "Value": 'Net Sales'
+			})
+			app.ajaxPost("/report/summarycalculatedatapivot", param, (res2) => {
+				let dataall2 = Lazy(res2.Data)
+					.groupBy((f) => f['year'])
+					.map((k, v) => {
+						return { _id: v, data: k }
+					})
+
+				rs.datascatter([])
+				let title = Lazy(rpt.optionDimensions()).findWhere({field: rs.breakdownBy()}).title
+				for (let i in dataall){
+					let currentDataAll = Lazy(dataall).findWhere({ _id: dataall[i]._id })
+					let currentDataAll2 = Lazy(dataall2).findWhere({ _id: dataall[i]._id })
+
+					let totalDataAll = Lazy(currentDataAll.data).sum((e) => e.value1)
+					let totalDataAll2 = Lazy(currentDataAll2.data).sum((e) => e.value1)
+
+					let maxNetSales = Lazy(currentDataAll2.data).max((e) => e.value1).value1
+					let percentage = totalDataAll / totalDataAll2
+					let v = maxNetSales * 100
+					let meanYear = (dataall[i].data.length/2).toFixed(0)-1
+					let titleValue = ''
+
+					for (let a in dataall[i].data){
+						titleValue = dataall[i].data[a][title]
+						if (a == meanYear)
+							titleValue = titleValue + '\n ' + dataall[i].data[a].year
+						rs.datascatter.push({
+							pplheader: percentage*100,
+							value1: dataall[i].data[a].value1/maxNetSales*100,
+							title: titleValue,
+							header: dataall[i].data[a].plmodel_plheader1,
+							year: dataall[i].data[a].year
+						})
+					}
+					if(i == 0){
+						rs.datascatter.push({
+							pplheader: null,
+							value1: null,
+							title: '',
+							header: null
+						})
+					}
+				}
+				rs.generateReport()
+			})
+		})
+	})
+}
+
+rs.generateReport = () => {
 	$(".scatter-view").kendoChart({
+		dataSource: {
+            data: rs.datascatter()
+        },
         title: {
             text: ""
         },
@@ -55,22 +124,26 @@ rs.generateReport = (data1, data2) => {
             // }
         },
         series: [{
-            name: "Gold Medals",
-            data: [40, 40, 40, 40, 40, 40, 40, null, 40, 40, 40, 40, 40, 40, 40],
-            color: "#f3ac32"
+            name: "PPL",
+            field: 'pplheader',
+            color: "#f3ac32",
+            tooltip: {
+				visible: true,
+				template: "#: dataItem.title # - #: kendo.toString(dataItem.pplheader, 'pplheader') #"
+			},
         }, {
-            name: "Aye",
-            data: [19, 25, 21, 26, 28, 31, 35, null, 60, 31, 34, 32, 24, 40, 38],
+            name: "PPL",
             color: "red",
-            // line: {
-            // 	opacity: 0,
-            // 	width: 0
-            // },
+            field: "value1",
             opacity: 0,
             markers : {
             	type: 'cross',
 				size: 8
-            }
+            },
+            tooltip: {
+				visible: true,
+				template: "#: dataItem.title # - #: kendo.toString(dataItem.value1, 'n2') #"
+			},
         }],
         valueAxis: {
             line: {
@@ -84,10 +157,13 @@ rs.generateReport = (data1, data2) => {
             }
         },
         categoryAxis: {
-            categories: ['aaa', 'aaaa', 'aaaa', 'aaaa \n 2015', '', '', '', '', '', '', '', '2016', '', '', '' ],
+            field: 'title',
             majorGridLines: {
                 visible: false
-            }
+            },
+      //       labels: {
+		    // 	rotation: 60
+		    // }
         },
         tooltip: {
             visible: true,
@@ -99,5 +175,6 @@ rs.generateReport = (data1, data2) => {
 $(() => {
 	rpt.value.From(moment("2015-02-02").toDate())
 	rpt.value.To(moment("2016-02-02").toDate())
-	rs.refresh()
+	// rs.refresh()
+	rs.getSalesHeaderList()
 })
