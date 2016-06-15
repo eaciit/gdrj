82b5ba7975f105e5ee227b3cae0e111ab306fa2d
novalagung <caknopal@gmail.com>
2016-06-15 23:06:33 +0700

    no message

--- web/assets/core/js/page-report-dashboard.js
+++ web/assets/core/js/page-report-dashboard.js
@@ -30,89 +30,14 @@ var dsbrd = viewModel.dashboard;
 
 dsbrd.data = ko.observableArray([]);
 dsbrd.columns = ko.observableArray([]);
-dsbrd.dimension = ko.observable('customer.channelname');
+dsbrd.breakdown = ko.observable('customer.channelname');
 dsbrd.fiscalYear = ko.observable(2014);
 dsbrd.contentIsLoading = ko.observable(false);
 
 dsbrd.refresh = function () {
-	var parse = function parse(res) {
-		var rows = [{ pnl: 'Gross Sales', plcodes: ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6"] }, { pnl: 'Growth', plcodes: [] }, // NOT YET
-		{ pnl: 'Sales Discount', plcodes: ["PL7", "PL8"] }, { pnl: 'ATL', plcodes: [] }, // NOT YET
-		{ pnl: 'BTL', plcodes: [] }, // NOT YET
-		{ pnl: "COGS", plcodes: ["PL74B"] }, { pnl: "Gross Margin", plcodes: ["PL74C"] }, { pnl: "SGA", plcodes: ["PL94A"] }, { pnl: "Royalties", plcodes: ["PL26"] }, { pnl: "EBITDA", plcodes: ["PL44C"] }, { pnl: "EBIT %", plcodes: [] }, { pnl: "EBIT", plcodes: ["PL44B"] }];
-
-		var columns = [{ field: 'pnl', title: 'PNL', attributes: { class: 'bold' } }];
-
-		var data = _.sortBy(res.Data.Data, function (d) {
-			return toolkit.redefine(d._id['_id_' + toolkit.replace(dsbrd.dimension(), '.', '_')], 'Other');
-		});
-
-		rows.forEach(function (d) {
-			data.forEach(function (e, i) {
-				var field = e._id['_id_' + toolkit.replace(dsbrd.dimension(), '.', '_')];
-				var key = 'field' + i;
-				d[key] = toolkit.sum(d.plcodes, function (f) {
-					return e[f];
-				});
-
-				if (d.pnl == 'EBIT %') {
-					var grossSales = rows.find(function (f) {
-						return f.pnl == 'Gross Sales';
-					});
-					var grossSalesValue = toolkit.sum(grossSales.plcodes, function (f) {
-						return e[f];
-					});
-
-					var ebit = rows.find(function (f) {
-						return f.pnl == 'EBIT';
-					});
-					var ebitValue = toolkit.sum(ebit.plcodes, function (f) {
-						return e[f];
-					});
-
-					console.log(field, grossSalesValue / ebitValue, kendo.toString(grossSalesValue / ebitValue, 'n2') + ' %');
-					d[key] = kendo.toString(toolkit.number(grossSalesValue / ebitValue), 'n2') + ' %';
-				}
-
-				if (toolkit.isDefined(columns.find(function (f) {
-					return f.field == key;
-				}))) {
-					return;
-				}
-
-				columns.push({
-					field: key,
-					title: toolkit.redefine(field, 'Other'),
-					format: '{0:n0}',
-					attributes: { class: 'align-right' },
-					headerAttributes: {
-						style: 'text-align: right !important;',
-						class: 'bold tooltipster',
-						title: 'Click to sort'
-					}
-				});
-			});
-		});
-
-		dsbrd.data(rows);
-		dsbrd.columns(columns);
-
-		if (columns.length > 5) {
-			columns.forEach(function (d, i) {
-				if (i == 0) {
-					d.width = 200;
-					d.locked = true;
-					return;
-				}
-
-				d.width = 150;
-			});
-		}
-	};
-
 	var param = {};
 	param.pls = ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6", "PL7", "PL8", "PL74B", "PL74C", "PL94A", "PL26", "PL44B", "PL44C"];
-	param.groups = [dsbrd.dimension()];
+	param.groups = [dsbrd.breakdown()];
 	param.aggr = 'sum';
 	param.filters = rpt.getFilterValue();
 	param.filters.push({
@@ -130,9 +55,8 @@ dsbrd.refresh = function () {
 				return;
 			}
 
-			parse(res);
 			dsbrd.contentIsLoading(false);
-			dsbrd.render();
+			dsbrd.render(res);
 		}, function () {
 			dsbrd.contentIsLoading(false);
 		});
@@ -142,7 +66,80 @@ dsbrd.refresh = function () {
 	fetch();
 };
 
-dsbrd.render = function () {
+dsbrd.render = function (res) {
+	var rows = [{ pnl: 'Gross Sales', plcodes: ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6"] }, { pnl: 'Growth', plcodes: [] }, // NOT YET
+	{ pnl: 'Sales Discount', plcodes: ["PL7", "PL8"] }, { pnl: 'ATL', plcodes: [] }, // NOT YET
+	{ pnl: 'BTL', plcodes: [] }, // NOT YET
+	{ pnl: "COGS", plcodes: ["PL74B"] }, { pnl: "Gross Margin", plcodes: ["PL74C"] }, { pnl: "SGA", plcodes: ["PL94A"] }, { pnl: "Royalties", plcodes: ["PL26"] }, { pnl: "EBITDA", plcodes: ["PL44C"] }, { pnl: "EBIT %", plcodes: [] }, { pnl: "EBIT", plcodes: ["PL44B"] }];
+
+	var columns = [{ field: 'pnl', title: 'PNL', attributes: { class: 'bold' } }];
+
+	var data = _.sortBy(res.Data.Data, function (d) {
+		return toolkit.redefine(d._id['_id_' + toolkit.replace(dsbrd.breakdown(), '.', '_')], 'Other');
+	});
+
+	rows.forEach(function (d) {
+		data.forEach(function (e, i) {
+			var field = e._id['_id_' + toolkit.replace(dsbrd.breakdown(), '.', '_')];
+			var key = 'field' + i;
+			d[key] = toolkit.sum(d.plcodes, function (f) {
+				return e[f];
+			});
+
+			if (d.pnl == 'EBIT %') {
+				var grossSales = rows.find(function (f) {
+					return f.pnl == 'Gross Sales';
+				});
+				var grossSalesValue = toolkit.sum(grossSales.plcodes, function (f) {
+					return e[f];
+				});
+
+				var ebit = rows.find(function (f) {
+					return f.pnl == 'EBIT';
+				});
+				var ebitValue = toolkit.sum(ebit.plcodes, function (f) {
+					return e[f];
+				});
+
+				console.log(field, grossSalesValue / ebitValue, kendo.toString(grossSalesValue / ebitValue, 'n2') + ' %');
+				d[key] = kendo.toString(toolkit.number(grossSalesValue / ebitValue), 'n2') + ' %';
+			}
+
+			if (toolkit.isDefined(columns.find(function (f) {
+				return f.field == key;
+			}))) {
+				return;
+			}
+
+			columns.push({
+				field: key,
+				title: toolkit.redefine(field, 'Other'),
+				format: '{0:n0}',
+				attributes: { class: 'align-right' },
+				headerAttributes: {
+					style: 'text-align: right !important;',
+					class: 'bold tooltipster',
+					title: 'Click to sort'
+				}
+			});
+		});
+	});
+
+	dsbrd.data(rows);
+	dsbrd.columns(columns);
+
+	if (columns.length > 5) {
+		columns.forEach(function (d, i) {
+			if (i == 0) {
+				d.width = 200;
+				d.locked = true;
+				return;
+			}
+
+			d.width = 150;
+		});
+	}
+
 	var fields = {};
 
 	if (dsbrd.data().length > 0) {
@@ -177,35 +174,15 @@ dsbrd.render = function () {
 viewModel.dashboardRanking = {};
 var rank = viewModel.dashboardRanking;
 
-rank.dimension = ko.observable('customer.channelname');
+rank.breakdown = ko.observable('customer.channelname');
 rank.columns = ko.observableArray([{ field: 'pnl', title: 'PNL', attributes: { class: 'bold' } }, { field: 'gmPercentage', title: 'GM %', type: 'percentage', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n2}' }, { field: 'cogsPercentage', title: 'COGS %', type: 'percentage', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n2}' }, { field: 'ebitPercentage', title: 'EBIT %', type: 'percentage', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n2}' }, { field: 'ebitdaPercentage', title: 'EBITDA %', type: 'percentage', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n2}' }, { field: 'netSales', title: 'Net Sales', type: 'number', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n0}' }, { field: 'ebit', title: 'EBIT', type: 'number', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n0}' }]);
 rank.contentIsLoading = ko.observable(false);
 rank.data = ko.observableArray([]);
 
 rank.refresh = function () {
-	var parse = function parse(res) {
-		var rows = [];
-		res.Data.Data.forEach(function (d) {
-			var row = {};
-			row.pnl = d._id['_id_' + toolkit.replace(rank.dimension(), '.', '_')];
-			if ($.trim(row.pnl) == '') {
-				row.pnl = 'Other';
-			}
-			row.gmPercentage = d.PL74C / d.PL8A;
-			row.cogsPercentage = d.PL74B / d.PL8A;
-			row.ebitPercentage = d.PL44B / d.PL8A;
-			row.ebitdaPercentage = d.PL44C / d.PL8A;
-			row.netSales = d.PL8A;
-			row.ebit = d.PL44B;
-			rows.push(row);
-		});
-
-		rank.data(rows);
-	};
-
 	var param = {};
 	param.pls = ["PL74C", "PL74B", "PL44B", "PL44C", "PL8A"];
-	param.groups = [rank.dimension()];
+	param.groups = [rank.breakdown()];
 	param.aggr = 'sum';
 	param.filters = rpt.getFilterValue();
 
@@ -218,9 +195,8 @@ rank.refresh = function () {
 				return;
 			}
 
-			parse(res);
 			rank.contentIsLoading(false);
-			rank.render();
+			rank.render(res);
 		}, function () {
 			rank.contentIsLoading(false);
 		});
@@ -230,7 +206,25 @@ rank.refresh = function () {
 	fetch();
 };
 
-rank.render = function () {
+rank.render = function (res) {
+	var rows = [];
+	res.Data.Data.forEach(function (d) {
+		var row = {};
+		row.pnl = d._id['_id_' + toolkit.replace(rank.breakdown(), '.', '_')];
+		if ($.trim(row.pnl) == '') {
+			row.pnl = 'Other';
+		}
+		row.gmPercentage = d.PL74C / d.PL8A;
+		row.cogsPercentage = d.PL74B / d.PL8A;
+		row.ebitPercentage = d.PL44B / d.PL8A;
+		row.ebitdaPercentage = d.PL44C / d.PL8A;
+		row.netSales = d.PL8A;
+		row.ebit = d.PL44B;
+		rows.push(row);
+	});
+
+	rank.data(rows);
+
 	var config = {
 		dataSource: {
 			data: rank.data(),
@@ -250,16 +244,28 @@ rank.render = function () {
 
 viewModel.salesDistribution = {};
 var sd = viewModel.salesDistribution;
+sd.contentIsLoading = ko.observable(false);
 
 sd.breakdown = ko.observable('customer.channelname');
-sd.data = ko.observableArray([{ customer_channelname: "modern trade", group: "hyper", percentage: 8, value: 240000 }, { customer_channelname: "modern trade", group: "super", percentage: 12, value: 360000 }, { customer_channelname: "modern trade", group: "mini", percentage: 10, value: 300000 }, { customer_channelname: "general trade", group: "type 1", percentage: 3, value: 90000 }, { customer_channelname: "general trade", group: "type 2", percentage: 4, value: 120000 }, { customer_channelname: "general trade", group: "type 3", percentage: 5, value: 150000 }, { customer_channelname: "general trade", group: "type 4", percentage: 4, value: 120000 }, { customer_channelname: "general trade", group: "type 5", percentage: 2, value: 60000 }, { customer_channelname: "general trade", group: "type 6", percentage: 6, value: 180000 }, { customer_channelname: "general trade", group: "type 7", percentage: 5, value: 150000 }, { customer_channelname: "general trade", group: "type 8", percentage: 5, value: 150000 }, { customer_channelname: "general trade", group: "type 9", percentage: 6, value: 180000 }, { customer_channelname: "retail distribution", group: "", percentage: 30, value: 900000 }]);
-sd.render = function () {
-	var dimension = toolkit.replace(sd.breakdown(), ".", "_");
-	var total = toolkit.sum(sd.data(), function (d) {
-		return d.value;
+sd.data = ko.observableArray([]);
+sd.render = function (res) {
+	var breakdown = toolkit.replace(sd.breakdown(), ".", "_");
+	var total = toolkit.sum(res.Data.Data, function (d) {
+		return d.PL8A;
 	});
+
+	var rows = res.Data.Data.map(function (d) {
+		var row = {};
+		row[breakdown] = d._id['_id_' + breakdown];
+		row.group = '';
+		row.percentage = d.PL8A / total * 100;
+		row.value = d.PL8A;
+		return row;
+	});
+	sd.data(rows);
+
 	var op1 = _.groupBy(sd.data(), function (d) {
-		return d[dimension];
+		return d[breakdown];
 	});
 	var op2 = _.map(op1, function (v, k) {
 		return { key: k, values: v };
@@ -271,17 +277,21 @@ sd.render = function () {
 	var height = 20 * maxRow.values.length;
 	var width = 200;
 
-	var container = $('.grid-sales-dist');
-	var table = toolkit.newEl('table').appendTo(container).height(height);
+	var container = $('.grid-sales-dist').empty();
+	var table = toolkit.newEl('table').addClass('width-full').appendTo(container).height(height);
 	var tr1st = toolkit.newEl('tr').appendTo(table);
 	var tr2nd = toolkit.newEl('tr').appendTo(table);
+
+	if (op2.length > 5) {
+		table.width(op2.length * width);
+	}
 
 	op2.forEach(function (d) {
 		var td1st = toolkit.newEl('td').appendTo(tr1st).width(width);
 		var sumPercentage = _.sumBy(d.values, function (e) {
 			return e.percentage;
 		});
-		td1st.html(d.key + '<br />' + sumPercentage + ' %');
+		td1st.html(d.key + '<br />' + kendo.toString(sumPercentage, 'n2') + ' %');
 
 		var td2nd = toolkit.newEl('td').appendTo(tr2nd);
 
@@ -295,8 +305,8 @@ sd.render = function () {
 
 		d.values.forEach(function (e) {
 			var tr = toolkit.newEl('tr').appendTo(innerTable);
-			toolkit.newEl('td').appendTo(tr).html(e[dimension]).height(height / d.values.length);
-			toolkit.newEl('td').appendTo(tr).html(e.percentage + ' %');
+			toolkit.newEl('td').appendTo(tr).html(e[breakdown]).height(height / d.values.length);
+			toolkit.newEl('td').appendTo(tr).html(kendo.toString(e.percentage, 'n2') + ' %');
 			toolkit.newEl('td').appendTo(tr).html(kendo.toString(e.value, 'n0'));
 		});
 	});
@@ -305,7 +315,30 @@ sd.render = function () {
 	var tdTotal = toolkit.newEl('td').addClass('align-center total').attr('colspan', op2.length).appendTo(trTotal).html(kendo.toString(total, 'n0'));
 };
 sd.refresh = function () {
-	sd.render();
+	var param = {};
+	param.pls = ["PL8A"];
+	param.groups = [sd.breakdown()];
+	param.aggr = 'sum';
+	param.filters = rpt.getFilterValue();
+
+	var fetch = function fetch() {
+		toolkit.ajaxPost("/report/getpnldatanew", param, function (res) {
+			if (res.Status == "NOK") {
+				setTimeout(function () {
+					return fetch;
+				}, 1000 * 5);
+				return;
+			}
+
+			sd.contentIsLoading(false);
+			sd.render(res);
+		}, function () {
+			sd.contentIsLoading(false);
+		});
+	};
+
+	sd.contentIsLoading(true);
+	fetch();
 };
 
 $(function () {
--- web/src/js/page-report-dashboard.js
+++ web/src/js/page-report-dashboard.js
@@ -39,87 +39,14 @@ let dsbrd = viewModel.dashboard
 
 dsbrd.data = ko.observableArray([])
 dsbrd.columns = ko.observableArray([])
-dsbrd.dimension = ko.observable('customer.channelname')
+dsbrd.breakdown = ko.observable('customer.channelname')
 dsbrd.fiscalYear = ko.observable(2014)
 dsbrd.contentIsLoading = ko.observable(false)
 
 dsbrd.refresh = () => {
-	let parse = (res) => {
-		let rows = [
-			{ pnl: 'Gross Sales', plcodes: ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6"] },
-			{ pnl: 'Growth', plcodes: [] }, // NOT YET
-			{ pnl: 'Sales Discount', plcodes: ["PL7", "PL8"] },
-			{ pnl: 'ATL', plcodes: [] }, // NOT YET
-			{ pnl: 'BTL', plcodes: [] }, // NOT YET
-			{ pnl: "COGS", plcodes: ["PL74B"] },
-			{ pnl: "Gross Margin", plcodes: ["PL74C"] },
-			{ pnl: "SGA", plcodes: ["PL94A"] },
-			{ pnl: "Royalties", plcodes: ["PL26"] },
-			{ pnl: "EBITDA", plcodes: ["PL44C"] },
-			{ pnl: "EBIT %", plcodes: [] },
-			{ pnl: "EBIT", plcodes: ["PL44B"] },
-		]
-
-		let columns = [
-			{ field: 'pnl', title: 'PNL', attributes: { class: 'bold' } },
-		]
-
-		let data = _.sortBy(res.Data.Data, (d) => toolkit.redefine(d._id[`_id_${toolkit.replace(dsbrd.dimension(), '.', '_')}`], 'Other'))
-
-		rows.forEach((d) => {
-			data.forEach((e, i) => {
-				let field = e._id[`_id_${toolkit.replace(dsbrd.dimension(), '.', '_')}`]
-				let key = `field${i}`
-				d[key] = toolkit.sum(d.plcodes, (f) => e[f])
-
-				if (d.pnl == 'EBIT %') {
-					let grossSales = rows.find((f) => f.pnl == 'Gross Sales')
-					let grossSalesValue = toolkit.sum(grossSales.plcodes, (f) =>  e[f])
-
-					let ebit = rows.find((f) => f.pnl == 'EBIT')
-					let ebitValue = toolkit.sum(ebit.plcodes, (f) =>  e[f])
-
-					console.log(field, grossSalesValue / ebitValue, `${kendo.toString(grossSalesValue / ebitValue, 'n2')} %`)
-					d[key] = `${kendo.toString(toolkit.number(grossSalesValue / ebitValue), 'n2')} %`
-				}
-
-				if (toolkit.isDefined(columns.find((f) => f.field == key))) {
-					return
-				}
-
-				columns.push({
-					field: key,
-					title: toolkit.redefine(field, 'Other'),
-					format: '{0:n0}',
-					attributes: { class: 'align-right' },
-					headerAttributes: { 
-						style: 'text-align: right !important;',
-						class: 'bold tooltipster',
-						title: 'Click to sort'
-					}
-				})
-			})
-		})
-
-		dsbrd.data(rows)
-		dsbrd.columns(columns)
-
-		if (columns.length > 5) {
-			columns.forEach((d, i) => {
-				if (i == 0) {
-					d.width = 200
-					d.locked = true
-					return
-				}
-
-				d.width = 150
-			})
-		}
-	}
-
 	let param = {}
 	param.pls = ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6", "PL7", "PL8", "PL74B", "PL74C", "PL94A", "PL26", "PL44B", "PL44C"]
-	param.groups = [dsbrd.dimension()]
+	param.groups = [dsbrd.breakdown()]
 	param.aggr = 'sum'
 	param.filters = rpt.getFilterValue()
 	param.filters.push({
@@ -135,9 +62,8 @@ dsbrd.refresh = () => {
 				return
 			}
 
-			parse(res)		
 			dsbrd.contentIsLoading(false)
-			dsbrd.render()
+			dsbrd.render(res)
 		}, () => {
 			dsbrd.contentIsLoading(false)
 		})
@@ -147,7 +73,78 @@ dsbrd.refresh = () => {
 	fetch()
 }
 
-dsbrd.render = () => {
+dsbrd.render = (res) => {
+	let rows = [
+		{ pnl: 'Gross Sales', plcodes: ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6"] },
+		{ pnl: 'Growth', plcodes: [] }, // NOT YET
+		{ pnl: 'Sales Discount', plcodes: ["PL7", "PL8"] },
+		{ pnl: 'ATL', plcodes: [] }, // NOT YET
+		{ pnl: 'BTL', plcodes: [] }, // NOT YET
+		{ pnl: "COGS", plcodes: ["PL74B"] },
+		{ pnl: "Gross Margin", plcodes: ["PL74C"] },
+		{ pnl: "SGA", plcodes: ["PL94A"] },
+		{ pnl: "Royalties", plcodes: ["PL26"] },
+		{ pnl: "EBITDA", plcodes: ["PL44C"] },
+		{ pnl: "EBIT %", plcodes: [] },
+		{ pnl: "EBIT", plcodes: ["PL44B"] },
+	]
+
+	let columns = [
+		{ field: 'pnl', title: 'PNL', attributes: { class: 'bold' } },
+	]
+
+	let data = _.sortBy(res.Data.Data, (d) => toolkit.redefine(d._id[`_id_${toolkit.replace(dsbrd.breakdown(), '.', '_')}`], 'Other'))
+
+	rows.forEach((d) => {
+		data.forEach((e, i) => {
+			let field = e._id[`_id_${toolkit.replace(dsbrd.breakdown(), '.', '_')}`]
+			let key = `field${i}`
+			d[key] = toolkit.sum(d.plcodes, (f) => e[f])
+
+			if (d.pnl == 'EBIT %') {
+				let grossSales = rows.find((f) => f.pnl == 'Gross Sales')
+				let grossSalesValue = toolkit.sum(grossSales.plcodes, (f) =>  e[f])
+
+				let ebit = rows.find((f) => f.pnl == 'EBIT')
+				let ebitValue = toolkit.sum(ebit.plcodes, (f) =>  e[f])
+
+				console.log(field, grossSalesValue / ebitValue, `${kendo.toString(grossSalesValue / ebitValue, 'n2')} %`)
+				d[key] = `${kendo.toString(toolkit.number(grossSalesValue / ebitValue), 'n2')} %`
+			}
+
+			if (toolkit.isDefined(columns.find((f) => f.field == key))) {
+				return
+			}
+
+			columns.push({
+				field: key,
+				title: toolkit.redefine(field, 'Other'),
+				format: '{0:n0}',
+				attributes: { class: 'align-right' },
+				headerAttributes: { 
+					style: 'text-align: right !important;',
+					class: 'bold tooltipster',
+					title: 'Click to sort'
+				}
+			})
+		})
+	})
+
+	dsbrd.data(rows)
+	dsbrd.columns(columns)
+
+	if (columns.length > 5) {
+		columns.forEach((d, i) => {
+			if (i == 0) {
+				d.width = 200
+				d.locked = true
+				return
+			}
+
+			d.width = 150
+		})
+	}
+
 	var fields = {}
 
     if (dsbrd.data().length > 0) {
@@ -187,7 +184,7 @@ dsbrd.render = () => {
 viewModel.dashboardRanking = {}
 let rank = viewModel.dashboardRanking
 
-rank.dimension = ko.observable('customer.channelname')
+rank.breakdown = ko.observable('customer.channelname')
 rank.columns = ko.observableArray([
 	{ field: 'pnl', title: 'PNL', attributes: { class: 'bold' } },
 	{ field: 'gmPercentage', title: 'GM %', type: 'percentage', attributes: { class: 'align-right' }, headerAttributes: { style: 'text-align: right !important;', class: 'bold tooltipster', title: 'Click to sort' }, format: '{0:n2}' },
@@ -201,29 +198,9 @@ rank.contentIsLoading = ko.observable(false)
 rank.data = ko.observableArray([])
 
 rank.refresh = () => {
-	let parse = (res) => {
-		let rows = []
-		res.Data.Data.forEach((d) => {
-			let row = {}
-			row.pnl = d._id[`_id_${toolkit.replace(rank.dimension(), '.', '_')}`]
-			if ($.trim(row.pnl) == '') {
-				row.pnl = 'Other'
-			}
-			row.gmPercentage = d.PL74C / d.PL8A
-			row.cogsPercentage = d.PL74B / d.PL8A
-			row.ebitPercentage = d.PL44B / d.PL8A
-			row.ebitdaPercentage = d.PL44C / d.PL8A
-			row.netSales = d.PL8A
-			row.ebit = d.PL44B
-			rows.push(row)
-		})
-
-		rank.data(rows)
-	}
-
 	let param = {}
 	param.pls = ["PL74C", "PL74B", "PL44B", "PL44C", "PL8A"]
-	param.groups = [rank.dimension()]
+	param.groups = [rank.breakdown()]
 	param.aggr = 'sum'
 	param.filters = rpt.getFilterValue()
 
@@ -234,9 +211,8 @@ rank.refresh = () => {
 				return
 			}
 
-			parse(res)		
 			rank.contentIsLoading(false)
-			rank.render()
+			rank.render(res)
 		}, () => {
 			rank.contentIsLoading(false)
 		})
@@ -246,7 +222,25 @@ rank.refresh = () => {
 	fetch()
 }
 
-rank.render = () => {
+rank.render = (res) => {
+	let rows = []
+	res.Data.Data.forEach((d) => {
+		let row = {}
+		row.pnl = d._id[`_id_${toolkit.replace(rank.breakdown(), '.', '_')}`]
+		if ($.trim(row.pnl) == '') {
+			row.pnl = 'Other'
+		}
+		row.gmPercentage = d.PL74C / d.PL8A
+		row.cogsPercentage = d.PL74B / d.PL8A
+		row.ebitPercentage = d.PL44B / d.PL8A
+		row.ebitdaPercentage = d.PL44C / d.PL8A
+		row.netSales = d.PL8A
+		row.ebit = d.PL44B
+		rows.push(row)
+	})
+
+	rank.data(rows)
+
 	let config = {
 		dataSource: {
 			data: rank.data(),
@@ -270,42 +264,44 @@ rank.render = () => {
 
 viewModel.salesDistribution = {}
 let sd = viewModel.salesDistribution
+sd.contentIsLoading = ko.observable(false)
 
 sd.breakdown = ko.observable('customer.channelname')
-sd.data = ko.observableArray([
-	{ customer_channelname: "modern trade", group: "hyper", percentage: 8, value: 240000 },
-	{ customer_channelname: "modern trade", group: "super", percentage: 12, value: 360000 },
-	{ customer_channelname: "modern trade", group: "mini", percentage: 10, value: 300000 },
-	{ customer_channelname: "general trade", group: "type 1", percentage: 3, value: 90000 },
-	{ customer_channelname: "general trade", group: "type 2", percentage: 4, value: 120000 },
-	{ customer_channelname: "general trade", group: "type 3", percentage: 5, value: 150000 },
-	{ customer_channelname: "general trade", group: "type 4", percentage: 4, value: 120000 },
-	{ customer_channelname: "general trade", group: "type 5", percentage: 2, value: 60000 },
-	{ customer_channelname: "general trade", group: "type 6", percentage: 6, value: 180000 },
-	{ customer_channelname: "general trade", group: "type 7", percentage: 5, value: 150000 },
-	{ customer_channelname: "general trade", group: "type 8", percentage: 5, value: 150000 },
-	{ customer_channelname: "general trade", group: "type 9", percentage: 6, value: 180000 },
-	{ customer_channelname: "retail distribution", group: "", percentage: 30, value: 900000 }
-])
-sd.render = () => {
-	let dimension = toolkit.replace(sd.breakdown(), ".", "_")
-	let total = toolkit.sum(sd.data(), (d) => d.value)
-	let op1 = _.groupBy(sd.data(), (d) => d[dimension])
+sd.data = ko.observableArray([])
+sd.render = (res) => {
+	let breakdown = toolkit.replace(sd.breakdown(), ".", "_")
+	let total = toolkit.sum(res.Data.Data, (d) => d.PL8A)
+
+	let rows = res.Data.Data.map((d) => {
+		let row = {}
+		row[breakdown] = d._id[`_id_${breakdown}`]
+		row.group = ''
+		row.percentage = d.PL8A / total * 100
+		row.value = d.PL8A
+		return row
+	})
+	sd.data(rows)
+
+	let op1 = _.groupBy(sd.data(), (d) => d[breakdown])
 	let op2 = _.map(op1, (v, k) => { return { key: k, values: v } })
 	let maxRow = _.maxBy(op2, (d) => d.values.length)
 	let maxRowIndex = op2.indexOf(maxRow)
 	let height = 20 * maxRow.values.length
 	let width = 200
 
-	let container = $('.grid-sales-dist')
-	let table = toolkit.newEl('table').appendTo(container).height(height)
+	let container = $('.grid-sales-dist').empty()
+	let table = toolkit.newEl('table').addClass('width-full').appendTo(container).height(height)
 	let tr1st = toolkit.newEl('tr').appendTo(table)
 	let tr2nd = toolkit.newEl('tr').appendTo(table)
+
+	if (op2.length > 5) {
+		table.width(op2.length * width)
+	}
 
 	op2.forEach((d) => {
 		let td1st = toolkit.newEl('td').appendTo(tr1st).width(width)
 		let sumPercentage = _.sumBy(d.values, (e) => e.percentage)
-		td1st.html(`${d.key}<br />${sumPercentage} %`)
+		td1st.html(`${d.key}<br />${kendo.toString(sumPercentage, 'n2')} %`)
 
 		let td2nd = toolkit.newEl('td').appendTo(tr2nd)
 
@@ -319,8 +315,8 @@ sd.render = () => {
 
 		d.values.forEach((e) => {
 			let tr = toolkit.newEl('tr').appendTo(innerTable)
-			toolkit.newEl('td').appendTo(tr).html(e[dimension]).height(height / d.values.length)
-			toolkit.newEl('td').appendTo(tr).html(`${e.percentage} %`)
+			toolkit.newEl('td').appendTo(tr).html(e[breakdown]).height(height / d.values.length)
+			toolkit.newEl('td').appendTo(tr).html(`${kendo.toString(e.percentage, 'n2')} %`)
 			toolkit.newEl('td').appendTo(tr).html(kendo.toString(e.value, 'n0'))
 		})
 	})
@@ -329,7 +325,28 @@ sd.render = () => {
 	let tdTotal = toolkit.newEl('td').addClass('align-center total').attr('colspan', op2.length).appendTo(trTotal).html(kendo.toString(total, 'n0'))
 }
 sd.refresh = () => {
-	sd.render()
+	let param = {}
+	param.pls = ["PL8A"]
+	param.groups = [sd.breakdown()]
+	param.aggr = 'sum'
+	param.filters = rpt.getFilterValue()
+
+	let fetch = () => {
+		toolkit.ajaxPost("/report/getpnldatanew", param, (res) => {
+			if (res.Status == "NOK") {
+				setTimeout(() => fetch, 1000 * 5)
+				return
+			}
+	
+			sd.contentIsLoading(false)
+			sd.render(res)
+		}, () => {
+			sd.contentIsLoading(false)
+		})
+	}
+
+	sd.contentIsLoading(true)
+	fetch()
 }
 
 $(() => {
