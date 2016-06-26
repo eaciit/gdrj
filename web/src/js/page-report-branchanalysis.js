8a005b6c61ca9cc6374d21f69cb5e0d64c52120b
novalagung <caknopal@gmail.com>
2016-06-26 20:48:51 +0700

    no message

--- web/src/js/page-report-branchanalysis.js
+++ web/src/js/page-report-branchanalysis.js
@@ -510,7 +510,7 @@ ba.clickExpand = (e) => {
 	let right = $(e).find('i.fa-chevron-right').length
 	let down = $(e).find('i.fa-chevron-down').length
 	if (right > 0){
-		if ($(e).hasClass('headerPL28')) {
+		if (['PL28', 'PL29A', 'PL31'].indexOf($(e).attr('idheaderpl')) > -1) {
 			$('.pivot-pnl .table-header').css('width', '530px')
 			$('.pivot-pnl .table-content').css('margin-left', '530px')
 		}
@@ -522,7 +522,7 @@ ba.clickExpand = (e) => {
 		$(`tr[statusvaltemp=hide]`).css('display', 'none')
 	}
 	if (down > 0) {
-		if ($(e).hasClass('headerPL28')) {
+		if (['PL28', 'PL29A', 'PL31'].indexOf($(e).attr('idheaderpl')) > -1) {
 			$('.pivot-pnl .table-header').css('width', '')
 			$('.pivot-pnl .table-content').css('margin-left', '')
 		}
