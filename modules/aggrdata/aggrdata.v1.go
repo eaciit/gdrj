9b386ef9a8fcc5f9d531a6595d8b34437654c578
alipsidikp <alip.sidik.p@gmail.com>
2016-06-28 01:31:38 +0700

    update aggr

--- modules/aggrdata/aggrdata.v1.go
+++ modules/aggrdata/aggrdata.v1.go
@@ -16,11 +16,10 @@ var conn dbox.IConnection
 var count int
 
 var (
-	t0                                         time.Time
-	fiscalyear, gscount, iscount, scount, step int
-	data                                       map[string]float64
-	mwg                                        sync.WaitGroup
-	mutex                                      = &sync.Mutex{}
+	t0         time.Time
+	fiscalyear int
+	data       map[string]float64
+	mutex      = &sync.Mutex{}
 )
 
 func setinitialconnection() {
@@ -50,44 +49,27 @@ func main() {
 
 	toolkit.Println("Start data query...")
 
-	filter := dbox.Eq("date.fiscal", toolkit.Sprintf("%d-%d", fiscalyear-1, fiscalyear))
-	c, _ := gdrj.Find(new(gdrj.SalesPL), filter, nil)
-	defer c.Close()
+	eperiode := time.Date(fiscalyear, 4, 1, 0, 0, 0, 0, time.UTC)
+	// speriode := eperiode.AddDate(-1, 0, 0)
 
-	scount = c.Count()
-	step = scount / 100
+	// filter := dbox.And(dbox.Gte("date.date", speriode), dbox.Lt("date.date", eperiode))
+	// csr, _ := conn.NewQuery().Select("date.fiscal", "pldatas").From("salespls-1").Where(filter).Cursor(nil)
+	// defer csr.Close()
 
-	jobs := make(chan map[string]*gdrj.PLData, count)
-	toolkit.Println("Prepare Worker")
-	for wi := 0; wi < 30; wi++ {
-		mwg.Add(1)
-		go worker(wi, jobs)
+	result := make(chan int, 12)
+	for i := 1; i <= 12; i++ {
+		tstart := eperiode.AddDate(0, -i, 0)
+		tend := tstart.AddDate(0, 1, 0)
+		toolkit.Sprintf("From : %v, To : %v", tstart, tend)
+
+		filter := dbox.And(dbox.Gte("date.date", tstart), dbox.Lt("date.date", tend))
+		go workerproc(i, filter, result)
 	}
 
-	for {
-		iscount++
-		spl := new(gdrj.SalesPL)
-		e := c.Fetch(spl, 1, false)
-		if e != nil {
-			toolkit.Println("EOF")
-			break
-		}
-
-		// for k, v := range spl.PLDatas {
-		// 	data[k] += v.Amount
-		// }
-
-		jobs <- spl.PLDatas
-
-		if iscount%step == 0 {
-			toolkit.Printfn("Sending %d of %d in %s", iscount, scount,
-				time.Since(t0).String())
-		}
-
+	for i := 1; i <= 12; i++ {
+		a := <-result
+		toolkit.Sprintf("Worker-%d Done in %s", a, time.Since(t0).String())
 	}
-
-	close(jobs)
-	mwg.Wait()
 
 	for k, v := range data {
 		toolkit.Printfn("%s,%v", k, v)
@@ -97,20 +79,48 @@ func main() {
 		time.Since(t0).String())
 }
 
-func worker(wi int, jobs <-chan map[string]*gdrj.PLData) {
-	defer mwg.Done()
-	var j map[string]*gdrj.PLData
-	for j = range jobs {
-		gscount++
-		for k, v := range j {
+func workerproc(wi int, filter *dbox.Filter, result chan<- int) {
+	workerconn, _ := modules.GetDboxIConnection("db_godrej")
+	defer workerconn.Close()
+
+	csr, _ := workerconn.NewQuery().Select("date.fiscal", "pldatas").From("salespls-1").Where(filter).Cursor(nil)
+	defer csr.Close()
+
+	scount := csr.Count()
+	iscount := 0
+	step := scount / 100
+
+	if step == 0 {
+		step = 1
+	}
+
+	for {
+		iscount++
+		spl := new(gdrj.SalesPL)
+		e := csr.Fetch(spl, 1, false)
+		if e != nil {
+			toolkit.Println("EOF")
+			break
+		}
+
+		fiscal := spl.Date.Fiscal
+		for k, v := range spl.PLDatas {
+			key := toolkit.Sprintf("%s_%s", fiscal, k)
 			mutex.Lock()
-			data[k] += v.Amount
+			data[key] += v.Amount
 			mutex.Unlock()
 		}
 
-		if gscount%step == 0 {
-			toolkit.Printfn("Processing %d of %d in %s", gscount, scount,
+		if iscount%step == 0 {
+			toolkit.Printfn("Go %d. Processing %d of %d (%d) in %s", wi, iscount, scount, iscount/step,
 				time.Since(t0).String())
 		}
+
+		if iscount == 20 {
+			break
+		}
+
 	}
+
+	result <- wi
 }
