e242cdbfc4d8d2355966d2a9c21f49006c33e4f0
novalagung <caknopal@gmail.com>
2016-06-28 23:19:35 +0700

    no message

--- web/src/js/page-report-analysis.js
+++ web/src/js/page-report-analysis.js
@@ -133,7 +133,7 @@ bkd.refresh = (useCache = false) => {
 
 			switch (bkd.breakdownBranch_ChannelRDNonRD()) {
 				case 'All':
-					values = rpt.masterData.Channel().map((d) => d._id)
+					values = []
 				break;
 				case 'RD':
 					values = ['I1']
@@ -145,11 +145,14 @@ bkd.refresh = (useCache = false) => {
 			}
 
 			param.groups.push('customer.channelname')
-			param.filters.push({
-				Field: 'customer.channelname',
-				Op: '$in',
-				Value: values
-			})
+			
+			if (values.length > 0) {
+				param.filters.push({
+					Field: 'customer.channelname',
+					Op: '$in',
+					Value: values
+				})
+			}
 		} else 
 
 		if (bkd.breakdownBranch_SubChannel() != '') {
@@ -500,6 +503,36 @@ bkd.buildStructure = (data) => {
 		}
 
 		if (bkd.breakdownBranch_ChannelRDNonRD() != '') {
+			let injectTotal = (data) => {
+				let renderTotalColumn = (d) => {
+					let totalColumn = {}
+					totalColumn._id = 'Total'
+					totalColumn.count = 1
+					totalColumn.excludeFromTotal = true
+
+					let totalSubColumn = {}
+					totalSubColumn._id = 'Total'
+					totalSubColumn.count = 1
+					totalSubColumn.excludeFromTotal = true
+
+					for (let p in d.subs[0]) {
+						if (d.subs[0].hasOwnProperty(p) && p.search('PL') > -1) {
+							totalColumn[p] = toolkit.sum(d.subs, (e) => e[p])
+							totalSubColumn[p] = toolkit.sum(d.subs, (e) => e[p])
+						}
+					}
+
+					totalColumn.subs = [totalSubColumn]
+					return totalColumn
+				}
+
+				data.forEach((d) => {
+					let totalColumn = renderTotalColumn(d)
+					d.subs = [totalColumn].concat(d.subs)
+					d.count = toolkit.sum(d.subs, (e) => e.count)
+				})
+			}
+
 			let parsed = groupThenMap(data, (d) => {
 				return d._id[`_id_customer_branchname`]
 			}).map((d) => {
@@ -518,6 +551,9 @@ bkd.buildStructure = (data) => {
 			})
 		
 			bkd.level(2)
+			// if (bkd.breakdownBranch_ChannelRDNonRD() == 'All') {
+			// 	injectTotal(parsed)
+			// }
 			let newParsed = _.orderBy(parsed, (d) => d.PL8A, 'desc')
 			return newParsed
 		}
@@ -843,255 +879,6 @@ bkd.render = () => {
 
 	// ========================= CONFIGURE THE HIRARCHY
 	rpt.buildGridLevels(rows)
-
-
-	return
-
-
-
-
-
-	
-	// let breakdowns = [bkd.breakdownBy()]
-	// let rows = []
-	
-	// let data = _.map(bkd.data(), (d) => {
-	// 	d.breakdowns = {}
-	// 	let titleParts = []
-
-	// 	breakdowns.forEach((e) => {
-	// 		let title = d._id[`_id_${toolkit.replace(e, '.', '_')}`]
-	// 		title = toolkit.whenEmptyString(title, '')
-	// 		d.breakdowns[e] = title
-	// 		titleParts.push(title)
-	// 	})
-		
-	// 	d._id = titleParts.join(' ')
-	// 	return d 
-	// })
-	
-	// let plmodels = _.sortBy(rpt.plmodels(), (d) => parseInt(d.OrderIndex.replace(/PL/g, '')))
-	// let exceptions = [
-	// 	"PL94C" /* "Operating Income" */, 
-	// 	"PL39B" /* "Earning Before Tax" */, 
-	// 	"PL41C" /* "Earning After Tax" */,
-	// ]
-	// let netSalesPLCode = 'PL8A'
-	// let netSalesPlModel = rpt.plmodels().find((d) => d._id == netSalesPLCode)
-	// let netSalesRow = {}, changeformula, formulayo
-
-	// rpt.fixRowValue(data)
-
-	// data.forEach((e) => {
-	// 	let breakdown = e._id
-	// 	let value = e[`${netSalesPlModel._id}`]; 
-	// 	value = toolkit.number(value)
-	// 	netSalesRow[breakdown] = value
-	// })
-
-	// data = _.orderBy(data, (d) => netSalesRow[d._id], 'desc')
-
-	// plmodels.forEach((d) => {
-	// 	let row = { PNL: d.PLHeader3, PLCode: d._id, PNLTotal: 0, Percentage: 0 }
-	// 	data.forEach((e) => {
-	// 		let breakdown = e._id
-	// 		let value = e[`${d._id}`]; 
-	// 		value = toolkit.number(value)
-	// 		row[breakdown] = value
-	// 		row.PNLTotal += value
-	// 	})
-	// 	data.forEach((e) => {
-	// 		let breakdown = e._id
-	// 		let percentage = toolkit.number(e[`${d._id}`] / row.PNLTotal) * 100; 
-	// 		percentage = toolkit.number(percentage)
-
-	// 		if (d._id != netSalesPLCode) {
-	// 			percentage = toolkit.number(row[breakdown] / netSalesRow[breakdown]) * 100
-	// 		}
-
-	// 		if (percentage < 0)
-	// 			percentage = percentage * -1
-
-	// 		row[`${breakdown} %`] = percentage
-	// 	})
-
-	// 	if (exceptions.indexOf(row.PLCode) > -1) {
-	// 		return
-	// 	}
-
-	// 	rows.push(row)
-	// })
-
-	// let TotalNetSales = _.find(rows, (r) => { return r.PLCode == "PL8A" }).PNLTotal
-	// rows.forEach((d, e) => {
-	// 	let TotalPercentage = (d.PNLTotal / TotalNetSales) * 100; 
-	// 	if (TotalPercentage < 0)
-	// 		TotalPercentage = TotalPercentage * -1
-	// 	rows[e].Percentage = TotalPercentage
-	// })
-
-	// let wrapper = toolkit.newEl('div')
-	// 	.addClass('pivot-pnl')
-	// 	.appendTo($('.breakdown-view'))
-
-	// let tableHeaderWrap = toolkit.newEl('div')
-	// 	.addClass('table-header')
-	// 	.appendTo(wrapper)
-
-	// let tableHeader = toolkit.newEl('table')
-	// 	.addClass('table')
-	// 	.appendTo(tableHeaderWrap)
-
-	// let tableContentWrap = toolkit.newEl('div')
-	// 	.appendTo(wrapper)
-	// 	.addClass('table-content')
-
-	// let tableContent = toolkit.newEl('table')
-	// 	.addClass('table')
-	// 	.appendTo(tableContentWrap)
-
-	// let trHeader1 = toolkit.newEl('tr')
-	// 	.appendTo(tableHeader)
-
-	// toolkit.newEl('th')
-	// 	.html('P&L')
-	// 	.appendTo(trHeader1)
-
-	// toolkit.newEl('th')
-	// 	.html('Total')
-	// 	.addClass('align-right')
-	// 	.appendTo(trHeader1)
-
-	// toolkit.newEl('th')
-	// 	.html('%')
-	// 	.addClass('align-right')
-	// 	.appendTo(trHeader1)
-
-	// let trContent1 = toolkit.newEl('tr')
-	// 	.appendTo(tableContent)
-
-	// let colWidth = 160
-	// let colPercentWidth = 60
-	// let totalWidth = 0
-	// let pnlTotalSum = 0
-
-	// if (bkd.breakdownBy() == "customer.branchname") {
-	// 	colWidth = 200
-	// }
-
-	// if (bkd.breakdownBy() == "customer.region") {
-	// 	colWidth = 230
-	// }
-
-	// data.forEach((d, i) => {
-	// 	if (d._id.length > 22)
-	// 		colWidth += 30
-	// 	toolkit.newEl('th')
-	// 		.html(d._id)
-	// 		.addClass('align-right')
-	// 		.appendTo(trContent1)
-	// 		.width(colWidth)
-
-	// 	toolkit.newEl('th')
-	// 		.html('%')
-	// 		.addClass('align-right cell-percentage')
-	// 		.appendTo(trContent1)
-	// 		.width(colPercentWidth)
-
-	// 	totalWidth += colWidth + colPercentWidth
-	// })
-	// // console.log('data ', data)
-
-	// tableContent.css('min-width', totalWidth)
-
-	// // console.log('row ', rows)
-	// rows.forEach((d, i) => {
-	// 	pnlTotalSum += d.PNLTotal
-
-	// 	let PL = d.PLCode
-	// 	PL = PL.replace(/\s+/g, '')
-	// 	let trHeader = toolkit.newEl('tr')
-	// 		.addClass(`header${PL}`)
-	// 		.attr(`idheaderpl`, PL)
-	// 		.appendTo(tableHeader)
-
-	// 	trHeader.on('click', () => {
-	// 		bkd.clickExpand(trHeader)
-	// 	})
-
-	// 	toolkit.newEl('td')
-	// 		.html('<i></i>' + d.PNL)
-	// 		.appendTo(trHeader)
-
-	// 	let pnlTotal = kendo.toString(d.PNLTotal, 'n0')
-	// 	toolkit.newEl('td')
-	// 		.html(pnlTotal)
-	// 		.addClass('align-right')
-	// 		.appendTo(trHeader)
-
-	// 	toolkit.newEl('td')
-	// 		.html(kendo.toString(d.Percentage, 'n2') + '%')
-	// 		.addClass('align-right')
-	// 		.appendTo(trHeader)
-
-	// 	let trContent = toolkit.newEl('tr')
-	// 		.addClass(`column${PL}`)
-	// 		.attr(`idpl`, PL)
-	// 		.appendTo(tableContent)
-
-	// 	data.forEach((e, f) => {
-	// 		let key = e._id
-	// 		let value = kendo.toString(d[key], 'n0')
-
-	// 		let percentage = kendo.toString(d[`${key} %`], 'n2')
-
-	// 		if ($.trim(value) == '') {
-	// 			value = 0
-	// 		}
-
-	// 		let cell = toolkit.newEl('td')
-	// 			.html(value)
-	// 			.addClass('align-right')
-	// 			.appendTo(trContent)
-
-	// 		cell.on('click', () => {
-	// 			bkd.renderDetail(d.PLCode, e.breakdowns)
-	// 		})
-
-	// 		toolkit.newEl('td')
-	// 			.html(`${percentage} %`)
-	// 			.addClass('align-right cell-percentage')
-	// 			.appendTo(trContent)
-	// 	})
-
-	// 	let boolStatus = false
-	// 	trContent.find('td').each((a,e) => {
-	// 		// console.log(trHeader.find('td:eq(0)').text(),$(e).text())
-	// 		if ($(e).text() != '0' && $(e).text() != '0.00 %') {
-	// 			boolStatus = true
-	// 		}
-	// 	})
-	// 	if (boolStatus) {
-	// 		trContent.attr('statusval', 'show')
-	// 		trHeader.attr('statusval', 'show')
-	// 	} else {
-	// 		trContent.attr('statusval', 'hide')
-	// 		trHeader.attr('statusval', 'hide')
-	// 	}
-	// })
-
-	// rpt.buildGridLevels(rows)
-
-	// setTimeout(() => {
-	// 	let newdata  = [], finddata
-	// 	$('.table-header tr.bold').each((a, e) => {
-	// 		finddata = _.find(rs.optionDimensionSelect(), (a) => { return a.field == $(e).attr('idheaderpl') })
-	// 		if (finddata != undefined)
-	// 			newdata.push(finddata)
-	// 	})
-	// 	newdata = _.sortBy(newdata, function(item) { return [item.name] })
-	// 	rs.optionDimensionSelect(newdata)
-	// })
 }
 
 bkd.optionBreakdownValues = ko.observableArray([])
