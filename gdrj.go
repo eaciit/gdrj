6ca3a6ae60fa0dfb73b536afa56c69a12a7e0ae8
novalagung <caknopal@gmail.com>
2016-06-28 20:38:15 +0700

    no message

--- gdrj.go
+++ gdrj.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	_ "eaciit/gdrj/web"
-	_ "eaciit/gdrj/web2"
+	web "eaciit/gdrj/web"
+	web2 "eaciit/gdrj/web2"
 
 	"eaciit/gdrj/web/installation"
 	"eaciit/gdrj/web/model"
+	"fmt"
 	"github.com/eaciit/knot/knot.v1"
 	"github.com/eaciit/toolkit"
-	// "net/http"
-	// "regexp"
-	// "strings"
+	"net/http"
+	"strings"
 )
 
 func main() {
@@ -26,9 +26,26 @@ func main() {
 	}
 
 	flagAddress := toolkit.Sprintf("localhost:%v", toolkit.ToString(port.Port))
+	appConf := knot.AppContainerConfig{Address: flagAddress}
+	otherRoutes := map[string]knot.FnContent{
+		"/": func(r *knot.WebContext) interface{} {
+			rURL := r.Request.URL.String()
+			prefix := web.AppName
+			if strings.Contains(rURL, web2.AppName) {
+				prefix = web2.AppName
+			}
+
+			sessionid := r.Session("sessionid", "")
+			if sessionid == "" {
+				http.Redirect(r.Writer, r.Request, fmt.Sprintf("/%s/page/login", prefix), 301)
+			} else {
+				http.Redirect(r.Writer, r.Request, fmt.Sprintf("/%s/page/dashboard", prefix), 301)
+			}
+
+			return true
+		},
+	}
 
 	knot.DefaultOutputType = knot.OutputTemplate
-	knot.StartContainer(&knot.AppContainerConfig{
-		Address: flagAddress,
-	})
+	knot.StartContainerWithFn(&appConf, otherRoutes)
 }
