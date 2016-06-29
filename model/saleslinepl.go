43b2129b56e2788a6833c42f93f642487edfbb1a
alipsidikp <alip.sidik.p@gmail.com>
2016-06-29 15:51:29 +0700

    .

--- model/saleslinepl.go
+++ model/saleslinepl.go
@@ -739,6 +739,39 @@ func (pl *SalesPL) CalcRoyalties(masters toolkit.M) {
 	pl.AddData("PL25", -royalsamount*2.85900895*pl.Ratio.Global, plmodels)
 }
 
+func (pl *SalesPL) CalcRoyalties2016(masters toolkit.M) {
+	if !masters.Has("royalties") {
+		return
+	}
+
+	netsalesamount := float64(0)
+	arrnetsales := []string{"PL1", "PL2", "PL3", "PL4", "PL5", "PL6", "PL7", "PL8", "PL7A"}
+	found := func(str string) bool {
+		for _, v := range arrnetsales {
+			if v == str {
+				return true
+			}
+		}
+		return false
+	}
+
+	aplmodel := pl.PLDatas
+	for k, v := range aplmodel {
+		if k == "PL25" {
+			delete(aplmodel, k)
+		}
+
+		if found(k) {
+			netsalesamount += v.Amount
+		}
+	}
+
+	pl.PLDatas = aplmodel
+
+	plmodels := masters.Get("plmodel").(map[string]*PLModel)
+	pl.AddData("PL25", -netsalesamount*2.85900895, plmodels)
+}
+
 //== OLD Fix Calculate Royalties
 // func (pl *SalesPL) CalcRoyalties(masters toolkit.M) {
 // 	if !masters.Has("royalties") {
