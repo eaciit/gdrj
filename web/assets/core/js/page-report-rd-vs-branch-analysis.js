dba02984241ccd81c40ba14f50eb0cf7d3cd8485
novalagung <caknopal@gmail.com>
2016-06-29 23:18:44 +0700

    no message

--- web/assets/core/js/page-report-rd-vs-branch-analysis.js
+++ web/assets/core/js/page-report-rd-vs-branch-analysis.js
@@ -40,6 +40,7 @@ v1.refresh = function () {
 	param.pls = [];
 	param.groups = rpt.parseGroups([v1.breakdownBy()]);
 	param.aggr = 'sum';
+	param.flag = 'branch-vs-rd';
 	param.filters = rpt.getFilterValue(false, v1.fiscalYear);
 
 	v1.contentIsLoading(true);
@@ -53,7 +54,7 @@ v1.refresh = function () {
 				return;
 			}
 
-			v1.data(v1.buildStructure(temp));
+			v1.data(v1.buildStructure(res.Data.Data));
 			rpt.plmodels(res.Data.PLModels);
 			v1.emptyGrid();
 			v1.contentIsLoading(false);
@@ -98,7 +99,7 @@ v1.clickExpand = function (e) {
 };
 
 v1.emptyGrid = function () {
-	$('.grid-total-branch-rd').replaceWith('<div class="breakdown-view ez grid-total-branch-rd"></div>');
+	$('.grid-breakdown-branch-rd').replaceWith('<div class="breakdown-view ez grid-breakdown-branch-rd"></div>');
 };
 
 v1.buildStructure = function (data) {
@@ -155,7 +156,7 @@ v1.buildStructure = function (data) {
 };
 
 v1.render = function () {
-	var container = $('.grid-total-branch-rd');
+	var container = $('.grid-breakdown-branch-rd');
 	if (v1.data().length == 0) {
 		container.html('No data found.');
 		return;
@@ -375,6 +376,7 @@ v2.refresh = function () {
 	param.pls = [];
 	param.groups = rpt.parseGroups([v2.breakdownBy()]);
 	param.aggr = 'sum';
+	param.flag = 'branch-vs-rd-only-mt-gt';
 	param.filters = rpt.getFilterValue(false, v2.fiscalYear);
 
 	v2.contentIsLoading(true);
@@ -388,7 +390,7 @@ v2.refresh = function () {
 				return;
 			}
 
-			v2.data(v2.buildStructure(temp));
+			v2.data(v2.buildStructure(res.Data.Data));
 			rpt.plmodels(res.Data.PLModels);
 			v2.emptyGrid();
 			v2.contentIsLoading(false);
@@ -433,7 +435,7 @@ v2.clickExpand = function (e) {
 };
 
 v2.emptyGrid = function () {
-	$('.grid-breakdown-branch-rd').replaceWith('<div class="breakdown-view ez grid-breakdown-branch-rd"></div>');
+	$('.grid-breakdown-channel').replaceWith('<div class="breakdown-view ez grid-breakdown-channel"></div>');
 };
 
 v2.buildStructure = function (data) {
@@ -490,7 +492,7 @@ v2.buildStructure = function (data) {
 };
 
 v2.render = function () {
-	var container = $('.grid-breakdown-branch-rd');
+	var container = $('.grid-breakdown-channel');
 	if (v2.data().length == 0) {
 		container.html('No data found.');
 		return;
@@ -691,11 +693,340 @@ v2.render = function () {
 	rpt.buildGridLevels(rows);
 };
 
+viewModel.RDvsBranchView3 = {};
+var v3 = viewModel.RDvsBranchView3;
+
+v3.contentIsLoading = ko.observable(false);
+
+v3.breakdownBy = ko.observable('customer.channelname');
+v3.breakdownByFiscalYear = ko.observable('date.fiscal');
+
+v3.data = ko.observableArray([]);
+v3.fiscalYear = ko.observable(rpt.value.FiscalYear());
+v3.level = ko.observable(1);
+
+v3.refresh = function () {
+	var useCache = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
+
+	var param = {};
+	param.pls = [];
+	param.groups = rpt.parseGroups([v3.breakdownBy()]);
+	param.aggr = 'sum';
+	param.flag = 'branch-vs-rd';
+	param.filters = rpt.getFilterValue(false, v3.fiscalYear);
+
+	v3.contentIsLoading(true);
+
+	var fetch = function fetch() {
+		toolkit.ajaxPost(viewModel.appName + "report/getpnldatanew", param, function (res) {
+			if (res.Status == "NOK") {
+				setTimeout(function () {
+					fetch();
+				}, 1000 * 5);
+				return;
+			}
+
+			v3.data(v3.buildStructure(res.Data.Data));
+			rpt.plmodels(res.Data.PLModels);
+			v3.emptyGrid();
+			v3.contentIsLoading(false);
+			v3.render();
+		}, function () {
+			v3.emptyGrid();
+			v3.contentIsLoading(false);
+		});
+	};
+
+	fetch();
+};
+
+v3.clickExpand = function (e) {
+	var right = $(e).find('i.fa-chevron-right').length;
+	var down = $(e).find('i.fa-chevron-down').length;
+	if (right > 0) {
+		if (['PL28', 'PL29A', 'PL31'].indexOf($(e).attr('idheaderpl')) > -1) {
+			$('.pivot-pnl .table-header').css('width', '530px');
+			$('.pivot-pnl .table-content').css('margin-left', '530px');
+		}
+
+		$(e).find('i').removeClass('fa-chevron-right');
+		$(e).find('i').addClass('fa-chevron-down');
+		$('tr[idparent=' + e.attr('idheaderpl') + ']').css('display', '');
+		$('tr[idcontparent=' + e.attr('idheaderpl') + ']').css('display', '');
+		$('tr[statusvaltemp=hide]').css('display', 'none');
+		rpt.refreshHeight(e.attr('idheaderpl'));
+	}
+	if (down > 0) {
+		if (['PL28', 'PL29A', 'PL31'].indexOf($(e).attr('idheaderpl')) > -1) {
+			$('.pivot-pnl .table-header').css('width', '');
+			$('.pivot-pnl .table-content').css('margin-left', '');
+		}
+
+		$(e).find('i').removeClass('fa-chevron-down');
+		$(e).find('i').addClass('fa-chevron-right');
+		$('tr[idparent=' + e.attr('idheaderpl') + ']').css('display', 'none');
+		$('tr[idcontparent=' + e.attr('idheaderpl') + ']').css('display', 'none');
+		rpt.hideAllChild(e.attr('idheaderpl'));
+	}
+};
+
+v3.emptyGrid = function () {
+	$('.grid-total-branch-rd').replaceWith('<div class="breakdown-view ez grid-total-branch-rd"></div>');
+};
+
+v3.buildStructure = function (data) {
+	var groupThenMap = function groupThenMap(data, group) {
+		var op1 = _.groupBy(data, function (d) {
+			return group(d);
+		});
+		var op2 = _.map(op1, function (v, k) {
+			var key = { _id: k, subs: v };
+			var sample = v[0];
+
+			var _loop3 = function _loop3(prop) {
+				if (sample.hasOwnProperty(prop) && prop != '_id') {
+					key[prop] = toolkit.sum(v, function (d) {
+						return d[prop];
+					});
+				}
+			};
+
+			for (var prop in sample) {
+				_loop3(prop);
+			}
+
+			return key;
+		});
+
+		return op2;
+	};
+
+	var parsed = groupThenMap(data, function (d) {
+		return 'Total ' + d._id._id_branchrd;
+	}).map(function (d) {
+		d.subs = _.orderBy(d.subs, function (e) {
+			return e.PL8A;
+		}, 'desc');
+		d.breakdowns = d.subs[0]._id;
+		d.count = 1;
+		return d;
+	});
+
+	v3.level(1);
+	var newParsed = _.orderBy(parsed, function (d) {
+		return d.PL8A;
+	}, 'desc');
+	return newParsed;
+};
+
+v3.render = function () {
+	var container = $('.grid-total-branch-rd');
+	if (v3.data().length == 0) {
+		container.html('No data found.');
+		return;
+	}
+
+	// ========================= TABLE STRUCTURE
+
+	var wrapper = toolkit.newEl('div').addClass('pivot-pnl-branch pivot-pnl').appendTo(container);
+
+	var tableHeaderWrap = toolkit.newEl('div').addClass('table-header').appendTo(wrapper);
+
+	var tableHeader = toolkit.newEl('table').addClass('table').appendTo(tableHeaderWrap);
+
+	var tableContentWrap = toolkit.newEl('div').appendTo(wrapper).addClass('table-content');
+
+	var tableContent = toolkit.newEl('table').addClass('table').appendTo(tableContentWrap);
+
+	var trHeader = toolkit.newEl('tr').appendTo(tableHeader);
+
+	toolkit.newEl('th').html('P&L').css('height', 34 * v3.level() + 'px').attr('data-rowspan', v3.level()).css('vertical-align', 'middle').addClass('cell-percentage-header').appendTo(trHeader);
+
+	toolkit.newEl('th').html('Total').css('height', 34 * v3.level() + 'px').attr('data-rowspan', v3.level()).css('vertical-align', 'middle').addClass('cell-percentage-header align-right').appendTo(trHeader);
+
+	toolkit.newEl('th').html('%').css('height', 34 * v3.level() + 'px').attr('data-rowspan', v3.level()).css('vertical-align', 'middle').addClass('cell-percentage-header align-right').appendTo(trHeader);
+
+	var trContents = [];
+	for (var i = 0; i < v3.level(); i++) {
+		trContents.push(toolkit.newEl('tr').appendTo(tableContent));
+	}
+
+	// ========================= BUILD HEADER
+
+	var data = v3.data();
+
+	var columnWidth = 120;
+	var totalColumnWidth = 0;
+	var pnlTotalSum = 0;
+	var dataFlat = [];
+	var percentageWidth = 80;
+
+	var countWidthThenPush = function countWidthThenPush(thheader, each, key) {
+		var currentColumnWidth = columnWidth;
+
+		each.key = key.join('_');
+		dataFlat.push(each);
+
+		totalColumnWidth += currentColumnWidth;
+		thheader.width(currentColumnWidth);
+	};
+
+	data.forEach(function (lvl1, i) {
+		var thheader1 = toolkit.newEl('th').html(lvl1._id).attr('colspan', lvl1.count).addClass('align-center').appendTo(trContents[0]);
+
+		if (v3.level() == 1) {
+			countWidthThenPush(thheader1, lvl1, [lvl1._id]);
+
+			totalColumnWidth += percentageWidth;
+			var thheader1p = toolkit.newEl('th').html('%').width(percentageWidth).addClass('align-center').appendTo(trContents[0]);
+
+			return;
+		}
+		thheader1.attr('colspan', lvl1.count * 2);
+
+		lvl1.subs.forEach(function (lvl2, j) {
+			var thheader2 = toolkit.newEl('th').html(lvl2._id).addClass('align-center').appendTo(trContents[1]);
+
+			if (v3.level() == 2) {
+				countWidthThenPush(thheader2, lvl2, [lvl1._id, lvl2._id]);
+
+				totalColumnWidth += percentageWidth;
+				var _thheader1p3 = toolkit.newEl('th').html('%').width(percentageWidth).addClass('align-center').appendTo(trContents[1]);
+
+				return;
+			}
+			thheader2.attr('colspan', lvl2.count);
+		});
+	});
+
+	tableContent.css('min-width', totalColumnWidth);
+
+	// ========================= CONSTRUCT DATA
+
+	var plmodels = _.sortBy(rpt.plmodels(), function (d) {
+		return parseInt(d.OrderIndex.replace(/PL/g, ''));
+	});
+	var exceptions = ["PL94C" /* "Operating Income" */, "PL39B" /* "Earning Before Tax" */, "PL41C" /* "Earning After Tax" */];
+	var netSalesPLCode = 'PL8A';
+	var netSalesRow = {};
+	var rows = [];
+
+	rpt.fixRowValue(dataFlat);
+
+	console.log("dataFlat", dataFlat);
+
+	dataFlat.forEach(function (e) {
+		var breakdown = e.key;
+		netSalesRow[breakdown] = e[netSalesPLCode];
+	});
+
+	plmodels.forEach(function (d) {
+		var row = { PNL: d.PLHeader3, PLCode: d._id, PNLTotal: 0, Percentage: 0 };
+		dataFlat.forEach(function (e) {
+			var breakdown = e.key;
+			var value = e['' + d._id];
+			row[breakdown] = value;
+
+			if (toolkit.isDefined(e.excludeFromTotal)) {
+				return;
+			}
+
+			row.PNLTotal += value;
+		});
+		dataFlat.forEach(function (e) {
+			var breakdown = e.key;
+			var percentage = toolkit.number(row[breakdown] / row.PNLTotal) * 100;
+			percentage = toolkit.number(percentage);
+
+			if (d._id != netSalesPLCode) {
+				percentage = toolkit.number(row[breakdown] / netSalesRow[breakdown]) * 100;
+			}
+
+			if (percentage < 0) percentage = percentage * -1;
+
+			row[breakdown + ' %'] = percentage;
+		});
+
+		if (exceptions.indexOf(row.PLCode) > -1) {
+			return;
+		}
+
+		rows.push(row);
+	});
+
+	console.log("rows", rows);
+
+	var TotalNetSales = _.find(rows, function (r) {
+		return r.PLCode == "PL8A";
+	}).PNLTotal;
+	rows.forEach(function (d, e) {
+		var TotalPercentage = d.PNLTotal / TotalNetSales * 100;
+		if (TotalPercentage < 0) TotalPercentage = TotalPercentage * -1;
+		rows[e].Percentage = TotalPercentage;
+	});
+
+	// ========================= PLOT DATA
+
+	rows.forEach(function (d, i) {
+		pnlTotalSum += d.PNLTotal;
+
+		var PL = d.PLCode;
+		PL = PL.replace(/\s+/g, '');
+		var trHeader = toolkit.newEl('tr').addClass('header' + PL).attr('idheaderpl', PL).attr('data-row', 'row-' + i).appendTo(tableHeader);
+
+		trHeader.on('click', function () {
+			v3.clickExpand(trHeader);
+		});
+
+		toolkit.newEl('td').html('<i></i>' + d.PNL).appendTo(trHeader);
+
+		var pnlTotal = kendo.toString(d.PNLTotal, 'n0');
+		toolkit.newEl('td').html(pnlTotal).addClass('align-right').appendTo(trHeader);
+
+		toolkit.newEl('td').html(kendo.toString(d.Percentage, 'n2') + ' %').addClass('align-right').appendTo(trHeader);
+
+		var trContent = toolkit.newEl('tr').addClass('column' + PL).attr('idpl', PL).attr('data-row', 'row-' + i).appendTo(tableContent);
+
+		dataFlat.forEach(function (e, f) {
+			var key = e.key;
+			var value = kendo.toString(d[key], 'n0');
+			var percentage = kendo.toString(d[key + ' %'], 'n2') + ' %';
+
+			if ($.trim(value) == '') {
+				value = 0;
+			}
+
+			var cell = toolkit.newEl('td').html(value).addClass('align-right').appendTo(trContent);
+
+			var cellPercentage = toolkit.newEl('td').html(percentage).addClass('align-right').appendTo(trContent);
+		});
+
+		var boolStatus = false;
+		trContent.find('td').each(function (a, e) {
+			if ($(e).text() != '0' && $(e).text() != '0.00 %') {
+				boolStatus = true;
+			}
+		});
+
+		if (boolStatus) {
+			trContent.attr('statusval', 'show');
+			trHeader.attr('statusval', 'show');
+		} else {
+			trContent.attr('statusval', 'hide');
+			trHeader.attr('statusval', 'hide');
+		}
+	});
+
+	// ========================= CONFIGURE THE HIRARCHY
+	rpt.buildGridLevels(rows);
+};
+
 vm.currentMenu('Analysis');
 vm.currentTitle('&nbsp;');
 vm.breadcrumb([{ title: 'Godrej', href: viewModel.appName + 'page/landing' }, { title: 'Home', href: viewModel.appName + 'page/landing' }, { title: 'Branch vs RD Analysis', href: '#' }]);
 
 $(function () {
+	v3.refresh();
 	v1.refresh();
 	v2.refresh();
 });
