7afbb1829747deb77d129e85563e7874cd8e1acc
novalagung <caknopal@gmail.com>
2016-07-13 21:34:15 +0700

    no message

--- web/src/js/page-report-growthanalysis.js
+++ web/src/js/page-report-growthanalysis.js
@@ -368,6 +368,7 @@ ag.optionSeries = ko.observableArray([
 	{ _id: 'PL44B|value', name: 'EBIT' },
 	{ _id: 'PL44B|percentage', name: 'EBIT Percent' },
 ])
+ag.panelNote = ko.observable('&nbsp;')
 
 ag.refresh = () => {
 	let param = {}
@@ -392,6 +393,7 @@ ag.refresh = () => {
 
 			ag.contentIsLoading(false)
 			ag.data(res.Data.Data)
+			ag.panelNote(`Growth <span class='color-red'>FY 2014-2015</span> to <span class='color-green'>FY 2014-2015</span>`)
 			ag.render()
 		}, () => {
 			ag.contentIsLoading(false)
@@ -544,7 +546,7 @@ ag.render = () => {
 		let max = _.max(op4.map((e) => _.max([e.series1, e.series2])))
 		let min = _.min(op4.map((e) => _.min([e.series1, e.series2])))
 		axes[0].max = toolkit.hardCeil(max)
-		axes[0].min = toolkit.hardFloor(min)
+		// axes[0].min = toolkit.hardFloor(min)
 	}
 
 	series.forEach((d, i) => {
@@ -849,6 +851,471 @@ ag.getBrandOrderByBrand = (brandname) => {
 
 
 
+viewModel.quarterGrowth = new Object()
+let qg = viewModel.quarterGrowth
+
+qg.contentIsLoading = ko.observable(false)
+qg.breakdownBy = ko.observable('customer.channelname')
+qg.series1PL = ko.observable('PL8A|value')
+qg.series2PL = ko.observable('PL44B|value')
+qg.limit = ko.observable(6)
+qg.data = ko.observableArray([])
+qg.optionQuarters = ko.observableArray([
+	"2014-2015 Q1",
+	"2014-2015 Q2",
+	"2014-2015 Q3",
+	"2014-2015 Q4",
+
+	"2015-2016 Q1",
+	"2015-2016 Q2",
+	"2015-2016 Q3",
+	"2015-2016 Q4"
+])
+qg.quarter1 = ko.observable('2014-2015 Q1')
+qg.quarter2 = ko.observable('2014-2015 Q2')
+qg.panelNote = ko.observable('&nbsp;')
+
+
+qg.refresh = () => {
+	let param = {}
+	param.pls = [qg.series1PL().split('|')[0], qg.series2PL().split('|')[0]]
+	param.groups = rpt.parseGroups([qg.breakdownBy(), 'date.quartertxt'])
+	param.aggr = 'sum'
+	param.filters = rpt.getFilterValue(true, ko.observableArray(rpt.optionFiscalYears()))
+
+	let fetch = () => {
+		toolkit.ajaxPost(viewModel.appName + "report/getpnldatanew", param, (res) => {
+			if (res.Status == "NOK") {
+				setTimeout(() => {
+					fetch()
+				}, 1000 * 5)
+				return
+			}
+
+			if (rpt.isEmptyData(res)) {
+				qg.contentIsLoading(false)
+				return
+			}
+
+			qg.contentIsLoading(false)
+			qg.data(res.Data.Data)
+			qg.panelNote(`Growth <span class='color-red'>${qg.quarter1()}</span> to <span class='color-green'>${qg.quarter2()}</span>`)
+			qg.render()
+		}, () => {
+			qg.contentIsLoading(false)
+		})
+	}
+
+	qg.contentIsLoading(true)
+	fetch()
+}
+
+qg.render = () => {
+	let series1PL = qg.series1PL().split('|')[0]
+	let series1Type = qg.series1PL().split('|')[1]
+	let series2PL = qg.series2PL().split('|')[0]
+	let series2Type = qg.series2PL().split('|')[1]
+
+	let billion = 1000000000
+	let quarters = [qg.quarter1(), qg.quarter2()]
+	let op0 = _.filter(qg.data(), (d) => quarters.indexOf(d._id._id_date_quartertxt) > -1)
+	let op1 = _.groupBy(op0, (d) => d._id[`_id_${toolkit.replace(qg.breakdownBy(), '.', '_')}`])
+	let op2 = _.map(op1, (v, k) => {
+		v = _.orderBy(v, (e) => quarters.indexOf(e._id._id_date_quartertxt), 'asc')
+
+		console.log('----v', v)
+
+		let o = {}
+		o.breakdown = k
+		o.series1 = 0
+		o.series2 = 0
+
+		toolkit.try(() => {
+			if (series1Type == 'percentage') {
+				o.series1 = (v[1][series1PL] - v[0][series1PL]) / v[0][series1PL] * 100
+			} else {
+				o.series1 = (v[1][series1PL] - v[0][series1PL]) / billion
+			}
+		})
+
+		toolkit.try(() => {
+			if (series2Type == 'percentage') {
+				o.series2 = (v[1][series2PL] - v[0][series2PL]) / v[0][series2PL] * 100
+			} else {
+				o.series2 = (v[1][series2PL] - v[0][series2PL]) / billion
+			}
+		})
+
+		return o
+	})
+	let op3 = _.orderBy(op2, (d) => {
+		let hack = parseInt('10 000 000 000 000'.replace(/\ /g, ''), 10)
+
+		if (qg.breakdownBy() == 'customer.channelname') {
+			let order = ag.getChannelOrderByChannelName(d.breakdown)
+			if (order > -1) {
+				return hack - order
+			}
+		} else
+
+		if (qg.breakdownBy() == 'product.brand') {
+			let order = ag.getBrandOrderByBrand(d.breakdown)
+			if (order > -1) {
+				return hack - order
+			}
+		}
+
+		return d.series1
+	}, 'desc')
+	let op4 = _.take(op3, qg.limit())
+
+	let width = $('#tab1').width()
+	if (_.min([qg.limit(), op4.length]) > 6) {
+		width = 160 * qg.limit()
+	}
+	if (width == $('#tab1').width()) {
+		width = `${width - 22}px`
+	}
+
+	let series = [{
+		field: 'series1',
+		name: (() => {
+			let row = ag.optionSeries().find((d) => d._id == qg.series1PL())
+			if (row != undefined) {
+				return row.name
+			}
+
+			return '&nbsp;'
+		})(),
+		axis: series1Type,
+		color: toolkit.seriesColorsGodrej[0]
+	}, {
+		field: 'series2',
+		name: (() => {
+			let row = ag.optionSeries().find((d) => d._id == qg.series2PL())
+			if (row != undefined) {
+				return row.name
+			}
+
+			return '&nbsp;'
+		})(),
+		axis: series2Type,
+		color: toolkit.seriesColorsGodrej[2]
+	}]
+
+	let axes = [{
+		name: series1Type,
+		majorGridLines: { color: '#fafafa' },
+        labels: { 
+			font: '"Source Sans Pro" 11px',
+        	format: "{0:n2}"
+        },
+    }]
+
+    let categoryAxis = {
+        field: 'breakdown',
+        labels: {
+			font: '"Source Sans Pro" 11px',
+        	format: "{0:n2}"
+        },
+		majorGridLines: { color: '#fafafa' }
+	}
+
+    if (series1Type != series2Type) {
+    	axes.push({
+			name: series2Type,
+			majorGridLines: { color: '#fafafa' },
+	        labels: { 
+				font: '"Source Sans Pro" 11px',
+	        	format: "{0:n2}"
+	        },
+	    })
+    }
+
+	if (axes.length > 1) {
+		categoryAxis.axisCrossingValue = [0, op4.length]
+
+		axes.forEach((d, i) => {
+			let s = toolkit.seriesColorsGodrej
+			d.color = [s[0], s[2]][i]
+
+			let orig = _.max(op4.map((f) => Math.abs(f[`series${i + 1}`])))
+			let max = Math.pow(10, String(parseInt(orig, 10)).length - 1) * (parseInt(String(parseInt(orig, 10))[0], 10) + 1)
+
+			d.min = (max * -1)
+			d.max = (max)
+
+			let seriesType = (i == 0) ? series1Type : series2Type
+			if (seriesType == 'percentage') {
+				d.labels.format = "{0:n1} %"
+			} else {
+				d.labels.format = "{0:n1} B"
+			}
+		})
+	} else {
+		let max = _.max(op4.map((e) => _.max([e.series1, e.series2])))
+		let min = _.min(op4.map((e) => _.min([e.series1, e.series2])))
+		axes[0].max = toolkit.hardCeil(max)
+		// axes[0].min = toolkit.hardFloor(min)
+	}
+
+	series.forEach((d, i) => {
+		let seriesType = (i == 0) ? series1Type : series2Type
+
+		d.tooltip = {
+			visible: true,
+			template: (e) => {
+				let value = `${kendo.toString(e.value, 'n1')} B`
+
+				let seriesType = (i == 0) ? series1Type : series2Type
+				if (seriesType == 'percentage') {
+					value = `${kendo.toString(e.value, 'n1')} %`
+				}
+
+				return `${d.name}: ${value}<br />Click to show detail`
+			}
+		}
+
+		d.labels = {
+			visible: true,
+		}
+
+		if (seriesType == 'percentage') {
+			d.labels.template = (g) => {
+				return `${g.series.name}\n${kendo.toString(g.value, 'n1')} %`
+			}
+		} else {
+			d.labels.template = (g) => {
+				return `${g.series.name}\n${kendo.toString(g.value, 'n1')} B`
+			}
+		}
+	})
+
+	let config = {
+		dataSource: { data: op4 },
+        legend: {
+            visible: true,
+            position: "bottom"
+        },
+        seriesDefaults: {
+            type: "column",
+            style: "smooth",
+            missingValues: "gap",
+			line: {
+				border: {
+					width: 1,
+					color: 'white'
+				},
+			},
+			overlay: { gradient: 'none' },
+			border: { width: 0 },
+        },
+		series: series,
+        valueAxis: axes,
+        categoryAxis: categoryAxis,
+        seriesClick: qg.seriesClick
+    }
+
+    $('.quarterly-diff').replaceWith(`<div class="quarterly-diff" style="width: ${width}px;"></div>`)
+    $('.quarterly-diff').kendoChart(config)
+    $('.quarterly-diff').data('kendoChart').bind('seriesClick', (e) => {
+    	if (qg.breakdownBy() == 'customer.channelname') {
+	    	let channelMap = {
+				"Modern Trade": "I3",
+				"General Trade": "I2",
+				"Regional Distributor": "I1",
+				"Industrial": "I4",
+				"Motorist": "I6",
+				"Export": "EXP",
+			}
+
+	    	qg.selectedData([channelMap[e.category]])
+    	} else {
+	    	qg.selectedData([e.category])
+    	}
+
+    	qg.modalDetailTitle(`Detail of ${e.category}`)
+    	qg.showDetailAs('value')
+    	qg.showDetail()
+    })
+}
+
+qg.popupIsLoading = ko.observable(false)
+qg.modalDetailTitle = ko.observable('Detail Growth')
+qg.showDetailAs = ko.observable('value')
+qg.selectedData = ko.observableArray([])
+
+
+qg.showDetail = () => {
+	qg.doRefreshDetail(true)
+}
+
+qg.refreshDetail = () => {
+	qg.doRefreshDetail(false)
+}
+
+qg.doRefreshDetail = (withModal = false) => {
+	let param = {}
+	param.pls = ag.detailPNL().map((d) => d.plcode)
+	param.groups = rpt.parseGroups([qg.breakdownBy()])
+	param.aggr = 'sum'
+	param.filters = rpt.getFilterValue(true, ko.observableArray(rpt.optionFiscalYears()))
+	param.filters.push({
+		Field: qg.breakdownBy(),
+		Op: '$in',
+		Value: qg.selectedData()
+	})
+
+	let fetch = () => {
+		toolkit.ajaxPost(viewModel.appName + "report/getpnldatanew", param, (res) => {
+			if (res.Status == "NOK") {
+				setTimeout(() => {
+					fetch()
+				}, 1000 * 5)
+				return
+			}
+
+			if (rpt.isEmptyData(res)) {
+				qg.popupIsLoading(false)
+				if (withModal) {
+					$('#modal-detail-quarter-diff').modal('hide')
+				}
+				return
+			}
+
+			qg.popupIsLoading(false)
+			if (withModal) {
+				toolkit.runAfter(() => {
+					qg.renderDetail(res.Data.Data)
+				}, 300)
+			} else {
+				qg.renderDetail(res.Data.Data)
+			}
+		}, () => {
+			qg.popupIsLoading(false)
+			if (withModal) {
+				$('#modal-detail-quarter-diff').modal('hide')
+			}
+		})
+	}
+
+	qg.popupIsLoading(true)
+	if (withModal) {
+		$('#modal-detail-quarter-diff').modal('show')
+	}
+
+	$('.chart-detail-quarter-diff').empty()
+	fetch()
+}
+
+qg.renderDetail = (data) => {
+	let billion = 1000000000
+
+	let values = []
+	let op1 = _.groupBy(data, (d) => d._id[`_id_${toolkit.replace(qg.breakdownBy(), '.', '_')}`])
+	let op2 = _.map(op1, (v, k) => {
+		let op3 = _.orderBy(v, (d) => d._id._id_date_fiscal, 'asc')
+		let o = {}
+		o.breakdown = k
+
+		ag.detailPNL().forEach((d) => {
+			o[d.plcode] = 0
+
+			toolkit.try(() => {
+				if (qg.showDetailAs() == 'value') {
+					o[d.plcode] = (op3[1][d.plcode] - op3[0][d.plcode]) / billion
+				} else {
+					o[d.plcode] = (op3[1][d.plcode] - op3[0][d.plcode]) / op3[0][d.plcode] * 100
+				}
+
+				o[d.plcode] = toolkit.number(o[d.plcode])
+				values.push(o[d.plcode])
+			})
+		})
+
+		return o
+	})
+
+	let series = ag.detailPNL()
+		// .filter((d) => d.sub == true)
+		.map((d) => ({
+			name: d.plname,
+			field: d.plcode,
+			tooltip: {
+				visible: true,
+				template: (e) => {
+					let suffix = (qg.showDetailAs() == 'value') ? 'B' : '%'
+					return `${e.series.name}: ${kendo.toString(e.value, 'n1')} ${suffix}`
+				}
+			},
+			labels: {
+				visible: true,
+				template: (e) => {
+					let suffix = (qg.showDetailAs() == 'value') ? 'B' : '%'
+					return `${e.series.name}\n${kendo.toString(e.value, 'n1')} ${suffix}`
+				}
+			},
+			color: (e) => ((e.value < 0) ? '#e7505a' : '#74B841')
+		}))
+
+	let max = toolkit.increaseXPercent(toolkit.hardCeil(_.max(values)), 10)
+	let min = toolkit.increaseXPercent(toolkit.hardFloor(_.min(values)), 5)
+
+	let config = {
+		dataSource: { data: op2 },
+        legend: {
+            visible: false,
+            position: "bottom"
+        },
+        seriesDefaults: {
+            type: "column",
+            style: "smooth",
+            missingValues: "gap",
+			line: {
+				border: {
+					width: 1,
+					color: 'white'
+				},
+			},
+			overlay: { gradient: 'none' },
+			border: { width: 0 },
+        },
+		series: series,
+        valueAxis: {
+			majorGridLines: { color: '#fafafa' },
+	        labels: { 
+				font: '"Source Sans Pro" 11px',
+	        	format: "{0:n2}"
+	        },
+	        min: min,
+	        max: max
+	    },
+        categoryAxis: {
+	        field: 'breakdown',
+	        labels: {
+				font: '"Source Sans Pro" 11px',
+	        	format: "{0:n2}",
+	        	padding: {
+	        		bottom: 40
+	        	}
+	        },
+			majorGridLines: { color: '#fafafa' }
+		}
+    }
+
+	console.log('---- data', data)
+	console.log('---- op2', op2)
+	console.log('---- config', config)
+
+    $('.chart-detail-quarter-diff').replaceWith(`<div class="chart-detail-quarter-diff" style="height: 300px;"></div>`)
+    $('.chart-detail-quarter-diff').kendoChart(config)
+}
+
+
+
+
+
+
 vm.currentMenu('Analysis')
 vm.currentTitle('&nbsp;')
 vm.breadcrumb([
@@ -860,6 +1327,8 @@ vm.breadcrumb([
 
 $(() => {
 	$('#modal-detail-annual-diff').appendTo($('body'))
+	$('#modal-detail-quarter-diff').appendTo($('body'))
 	grw.refresh()
 	ag.refresh()
+	qg.refresh()
 })
