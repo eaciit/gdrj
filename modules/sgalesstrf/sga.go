af27521aac9e0c9c6b6cec79bc6ae8b0c1d5abb9
juragan360 <ariefda@hotmail.com>
2016-07-20 19:01:52 +0700

    multiexec

--- modules/sgalesstrf/sga.go
+++ modules/sgalesstrf/sga.go
@@ -90,10 +90,11 @@ func buildratio() {
 func processTable() {
 	connsave, _ := modules.GetDboxIConnection("db_godrej")
 	defer connsave.Close()
-	qsave := connsave.NewQuery().From(desttablename).Save()
+	qsave := connsave.NewQuery().SetConfig("multiexec", true).From(desttablename).Save()
 
 	connselect, _ := modules.GetDboxIConnection("db_godrej")
 	defer connselect.Close()
+
 	cursor, _ := connselect.NewQuery().From(calctablename).Select().Cursor(nil)
 	defer cursor.Close()
 
