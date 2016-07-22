0d960531137df71b88733e02063d02b4f30cafef
novalagung <caknopal@gmail.com>
2016-07-22 18:05:39 +0700

    no message

--- web/src/js/page-report-dynamic.js
+++ web/src/js/page-report-dynamic.js
@@ -106,7 +106,7 @@ rd.render = () => {
 			visible: true,
 			template: (e) => {
 				let val = kendo.toString(e.value, 'n1')
-				return `${e.series.name} - ${e.category} : ${val}`
+				return `${e.series.name} : ${val}`
 			}
 		}
 		o.labels = {
@@ -142,7 +142,7 @@ rd.render = () => {
         	format: "{0:n2}"
         },
 		majorGridLines: { color: '#fafafa' },
-		axisCrossingValues: [0, 0, op3.length]
+		axisCrossingValues: [op3.length, 0, 0]
 	}
 
 	let config = {
@@ -165,7 +165,7 @@ rd.render = () => {
 			border: { width: 0 },
         },
 		series: series,
-        valueAxis: axes,
+        valueAxis: axes.reverse(),
         categoryAxis: categoryAxis
     }
 
@@ -175,44 +175,241 @@ rd.render = () => {
     $('.report').kendoChart(config)
 }
 
-rd.configure = (config) => toolkit.noop
+rd.configure = (config) => {
+	config.series[2].labels.template = (e) => {
+		let val = kendo.toString(e.value, 'n1')
+		return `${val} %`
+	}
+}
 
 rd.getQueryStringValue = (key) => {
 	return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"))
 }  
 
 rd.setup = () => {
+	rd.breakdownBy('customer.channelname')
+
 	switch (rd.getQueryStringValue('p')) {
 		case 'sales-return-rate': {
 			vm.currentTitle('Sales Return Rate')
-			rd.breakdownBy('customer.channelname')
 			rd.series = ko.observableArray([{ 
 				_id: 'salesreturn', 
 				plheader: 'Sales Return',
 				callback: (v, k) => {
-					return Math.abs(toolkit.sum(v, (e) => e.salesreturn)) / rd.divider()
+					return toolkit.number(Math.abs(toolkit.sum(v, (e) => e.salesreturn)) / rd.divider())
 				}
 			}, { 
 				_id: 'salesrevenue', 
 				plheader: 'Sales Revenue',
 				callback: 'PL8A'
 			}, { 
-				_id: 'salesreturnrate', 
-				plheader: 'Sales Return Rate',
+				_id: 'prcnt', 
+				plheader: vm.currentTitle(),
 				callback: (v, k) => {
 					let salesreturn = Math.abs(toolkit.sum(v, (e) => e.salesreturn)) / rd.divider()
 					let netsales = Math.abs(toolkit.sum(v, (e) => e.PL8A)) / rd.divider()
-					return salesreturn / netsales * 100
+					return toolkit.number(salesreturn / netsales) * 100
 				}
 			}])
+		} break;
+
+
+
+		case 'sales-discount-by-gross-sales': {
+			vm.currentTitle('Sales Discount by Gross Sales')
+			rd.series = ko.observableArray([{ 
+				_id: 'salesdiscount', 
+				plheader: 'Sales Discount',
+				callback: (v, k) => {
+					let salesDiscount = Math.abs(toolkit.sum(v, (e) => (e) => 
+						toolkit.sum(['PL7', 'PL8'], (f) => toolkit.number(e[f]))
+					))
+
+					return salesDiscount / rd.divider()
+				}
+			}, { 
+				_id: 'grosssales', 
+				plheader: 'Gross Sales',
+				callback: 'PL0'
+			}, { 
+				_id: 'prcnt', 
+				plheader: vm.currentTitle(),
+				callback: (v, k) => {
+					let salesDiscount = Math.abs(toolkit.sum(v, (e) => (e) => 
+						toolkit.sum(['PL7', 'PL8'], (f) => toolkit.number(e[f]))
+					))
+					let grossSales = Math.abs(toolkit.sum(v, (e) => e.PL0))
+
+					return toolkit.number(salesDiscount / grossSales) * 100
+				}
+			}])
+		} break;
+
+
+
+		case 'gross-sales-by-qty': {
+			vm.currentTitle('Gross Sales / Qty')
+			rd.divideBy('v1000000')
+			rd.series = ko.observableArray([{ 
+				_id: 'grosssales', 
+				plheader: 'Gross Sales',
+				callback: 'PL0'
+			}, { 
+				_id: 'salesqty', 
+				plheader: 'Quantity',
+				callback: (v, k) => {
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+					return quantity / rd.divider()
+				}
+			}, { 
+				_id: 'prcnt', 
+				plheader: vm.currentTitle(),
+				callback: (v, k) => {
+					let grossSales = Math.abs(toolkit.sum(v, (e) => e.PL0))
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+
+					return toolkit.number(grossSales / quantity) * 100
+				}
+			}])
+
 			rd.configure = (config) => {
 				config.series[2].labels.template = (e) => {
 					let val = kendo.toString(e.value, 'n1')
 					return `${val} %`
-					// return `${e.series.name}\n${val} %`
 				}
 			}
 		} break;
+
+
+
+		case 'discount-by-qty': {
+			vm.currentTitle('Discount / Qty')
+			rd.divideBy('v1000000')
+			rd.series = ko.observableArray([{ 
+				_id: 'salesdiscount', 
+				plheader: 'Sales Discount',
+				callback: (v, k) => {
+					let salesDiscount = Math.abs(toolkit.sum(v, (e) => (e) => 
+						toolkit.sum(['PL7', 'PL8'], (f) => toolkit.number(e[f]))
+					))
+
+					return salesDiscount / rd.divider()
+				}
+			}, { 
+				_id: 'salesqty', 
+				plheader: 'Quantity',
+				callback: (v, k) => {
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+					return quantity / rd.divider()
+				}
+			}, { 
+				_id: 'prcnt', 
+				plheader: vm.currentTitle(),
+				callback: (v, k) => {
+					let salesDiscount = Math.abs(toolkit.sum(v, (e) => (e) => 
+						toolkit.sum(['PL7', 'PL8'], (f) => toolkit.number(e[f]))
+					))
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+
+					return toolkit.number(salesDiscount / quantity) * 100
+				}
+			}])
+
+			rd.configure = (config) => {
+				config.series[2].labels.template = (e) => {
+					let val = kendo.toString(e.value, 'n1')
+					return `${val} %`
+				}
+			}
+		} break;
+
+
+
+		case 'net-price-by-qty': {
+			vm.currentTitle('Net Price / Qty')
+			rd.divideBy('v1000000')
+			rd.series = ko.observableArray([{ 
+				_id: 'netprice', 
+				plheader: 'Net Price',
+				callback: (v, k) => {
+					let amount = Math.abs(toolkit.sum(v, (e) => e.netamount))
+
+					return amount / rd.divider()
+				}
+			}, { 
+				_id: 'salesqty', 
+				plheader: 'Quantity',
+				callback: (v, k) => {
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+					return quantity / rd.divider()
+				}
+			}, { 
+				_id: 'prcnt', 
+				plheader: vm.currentTitle(),
+				callback: (v, k) => {
+					let amount = Math.abs(toolkit.sum(v, (e) => e.netamount))
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+
+					return toolkit.number(amount / quantity) * 100
+				}
+			}])
+
+			rd.configure = (config) => {
+				config.series[2].labels.template = (e) => {
+					let val = kendo.toString(e.value, 'n1')
+					return `${val} %`
+				}
+			}
+		} break;
+
+
+
+		case 'btl-by-qty': {
+			vm.currentTitle('BTL / Qty')
+			rd.divideBy('v1000000')
+			rd.series = ko.observableArray([{ 
+				_id: 'btl', 
+				plheader: 'BTL',
+				callback: (v, k) => {
+					let btl = Math.abs(toolkit.sum(v, (e) => 
+						toolkit.sum(['PL29', 'PL30', 'PL31', 'PL32'], (f) => toolkit.number(e[f]))
+					))
+
+					return btl / rd.divider()
+				}
+			}, { 
+				_id: 'salesqty', 
+				plheader: 'Quantity',
+				callback: (v, k) => {
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+					return quantity / rd.divider()
+				}
+			}, { 
+				_id: 'prcnt', 
+				plheader: vm.currentTitle(),
+				callback: (v, k) => {
+					let btl = Math.abs(toolkit.sum(v, (e) => 
+						toolkit.sum(['PL29', 'PL30', 'PL31', 'PL32'], (f) => toolkit.number(e[f]))
+					))
+					let quantity = Math.abs(toolkit.sum(v, (e) => e.salesqty))
+
+					return toolkit.number(btl / quantity) * 100
+				}
+			}])
+
+			rd.configure = (config) => {
+				config.series[2].labels.template = (e) => {
+					let val = kendo.toString(e.value, 'n1')
+					return `${val} %`
+				}
+			}
+		} break;
+
+
+		// ["PL29", "PL30", "PL31", "PL32"]
+
+
+
 		default: {
 			location.href = viewModel.appName + "page/report"
 		} break;
