7b76f118bde9e5a2b99fb3ccad74c549716e9051
alipsidikp <alip.sidik.p@gmail.com>
2016-08-02 12:32:26 +0700

    .

--- modules/aggrdata/aggrdata.v3tocollectioncogs.go
+++ modules/aggrdata/aggrdata.v3tocollectioncogs.go
@@ -63,7 +63,7 @@ func main() {
 
 	eperiode := time.Date(fiscalyear, 4, 1, 0, 0, 0, 0, time.UTC)
 	speriode := eperiode.AddDate(-1, 0, 0)
-	speriode = eperiode.AddDate(0, -1, 0)
+	speriode = eperiode.AddDate(0, 0, -2)
 
 	seeds := make([]time.Time, 0, 0)
 	seeds = append(seeds, speriode)
