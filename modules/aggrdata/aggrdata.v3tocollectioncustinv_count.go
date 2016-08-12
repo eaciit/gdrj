d1a55fe73a6d3a9d16b9e2bdcde6c9f4ad6bdf17
alipsidikp <alip.sidik.p@gmail.com>
2016-08-12 09:34:06 +0700

    cust inv count

--- modules/aggrdata/aggrdata.v3tocollectioncustinv_count.go
+++ modules/aggrdata/aggrdata.v3tocollectioncustinv_count.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"flag"
-	// "strings"
+	"strings"
 
 	"github.com/eaciit/dbox"
 	"github.com/eaciit/toolkit"
@@ -149,7 +149,7 @@ func workerproc(wi int, filters <-chan *dbox.Filter, result chan<- toolkit.M) {
 	filter := new(dbox.Filter)
 	for filter = range filters {
 		csr, _ := workerconn.NewQuery().Select("grossamount", "netamount", "discountamount", "salesqty", "date", "customer",
-			"product", "trxsrc", "source", "ref").
+			"product", "pldatas", "trxsrc", "source", "ref").
 			From(tablename).
 			Where(filter).
 			Cursor(nil)
@@ -164,6 +164,16 @@ func workerproc(wi int, filters <-chan *dbox.Filter, result chan<- toolkit.M) {
 		i := 0
 		t1 := time.Now()
 
+		arrpl := []string{"PL0", "PL1", "PL7", "PL2", "PL8", "PL3", "PL4", "PL5", "PL6", "PL6A", "PL7A", "PL8A"}
+		inlist := func(str string) bool {
+			for _, v := range arrpl {
+				if v == str {
+					return true
+				}
+			}
+			return false
+		}
+
 		for {
 			i++
 			tv := float64(0)
@@ -174,11 +184,14 @@ func workerproc(wi int, filters <-chan *dbox.Filter, result chan<- toolkit.M) {
 				break
 			}
 
-			key := toolkit.Sprintf("%s|%s|%d|%d|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s", spl.Date.Fiscal, spl.Date.QuarterTxt, spl.Date.Month,
-				spl.Date.Year, spl.Customer.BranchID, spl.Customer.BranchName, spl.Customer.KeyAccount, spl.Customer.ChannelID, spl.Customer.ChannelName,
-				spl.Customer.ReportChannel, spl.Customer.ReportSubChannel, spl.Customer.Zone, spl.Customer.Region,
+			arrid := strings.Split(spl.ID, "_")
+
+			key := toolkit.Sprintf("%s|%s|%s|%d|%d|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s",
+				arrid[0], spl.Date.Fiscal, spl.Date.QuarterTxt, spl.Date.Month, spl.Date.Year,
+				spl.Customer.ID, spl.Customer.BranchID, spl.Customer.BranchName, spl.Customer.KeyAccount, spl.Customer.ChannelID,
+				spl.Customer.ChannelName, spl.Customer.ReportChannel, spl.Customer.ReportSubChannel, spl.Customer.Zone, spl.Customer.Region,
 				spl.Customer.AreaName, spl.Customer.CustomerGroup, spl.Customer.CustomerGroupName, spl.Customer.CustType,
-				spl.Product.ID, spl.Product.Name, spl.Product.Brand, spl.TrxSrc, spl.Source, spl.Ref)
+				spl.TrxSrc, spl.Source, spl.Ref)
 
 			ktkm := toolkit.M{}
 			ktkm.Set("date_fiscal", spl.Date.Fiscal)
@@ -186,6 +199,9 @@ func workerproc(wi int, filters <-chan *dbox.Filter, result chan<- toolkit.M) {
 			ktkm.Set("date_month", spl.Date.Month)
 			ktkm.Set("date_year", spl.Date.Year)
 
+			ktkm.Set("trans_invoice", arrid[0])
+
+			ktkm.Set("customer_customerid", spl.Customer.ID)
 			ktkm.Set("customer_branchid", spl.Customer.BranchID)
 			ktkm.Set("customer_branchname", spl.Customer.BranchName)
 			ktkm.Set("customer_keyaccount", spl.Customer.KeyAccount)
@@ -200,9 +216,9 @@ func workerproc(wi int, filters <-chan *dbox.Filter, result chan<- toolkit.M) {
 			ktkm.Set("customer_customergroupname", spl.Customer.CustomerGroupName)
 			ktkm.Set("customer_custtype", spl.Customer.CustType)
 
-			ktkm.Set("product_brand", spl.Product.Brand)
-			ktkm.Set("product_skuid", spl.Product.ID)
-			ktkm.Set("product_name", spl.Product.Name)
+			// ktkm.Set("product_brand", spl.Product.Brand)
+			// ktkm.Set("product_skuid", spl.Product.ID)
+			// ktkm.Set("product_name", spl.Product.Name)
 
 			ktkm.Set("trxsrc", spl.TrxSrc)
 			ktkm.Set("source", spl.Source)
@@ -227,13 +243,16 @@ func workerproc(wi int, filters <-chan *dbox.Filter, result chan<- toolkit.M) {
 			tv = spl.SalesQty + dtkm.GetFloat64("salesqty")
 			dtkm.Set("salesqty", tv)
 
-			tv = 1 + dtkm.GetFloat64("salescount")
-			dtkm.Set("salescount", tv)
+			tkm.Set("salescount", 1)
+			// dtkm.Set("salescount", tv)
 
-			// for k, v := range spl.PLDatas {
-			// 	tv := v.Amount + dtkm.GetFloat64(k)
-			// 	dtkm.Set(k, tv)
-			// }
+			for k, v := range spl.PLDatas {
+				if !inlist(k) {
+					continue
+				}
+				tv := v.Amount + dtkm.GetFloat64(k)
+				dtkm.Set(k, tv)
+			}
 
 			tkm.Set(key, dtkm)
 
@@ -255,7 +274,7 @@ func workersave(wi int, jobs <-chan toolkit.M, result chan<- int) {
 	defer workerconn.Close()
 
 	qSave := workerconn.NewQuery().
-		From("salespls-summary-4cogssga_count").
+		From("salespls-summary-4custinv_count").
 		SetConfig("multiexec", true).
 		Save()
 
